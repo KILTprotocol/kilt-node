d204852eec2619bc012a108cfde20008b83dfd03
Gav <gavin@parity.io>
2018-10-28 14:16:12 +0100

    Fix for latest APIs

--- runtime/src/demo.rs
+++ runtime/src/demo.rs
@@ -3,15 +3,37 @@
 
 use parity_codec::Encode;
 use srml_support::{StorageValue, dispatch::Result};
-use runtime_primitives::traits::{Hash, OnFinalise};
+use runtime_primitives::traits::Hash;
 use {balances, system::{self, ensure_signed}};
 
 pub trait Trait: balances::Trait {}
 
 decl_module! {
 	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
-		fn play(origin) -> Result;
-		fn set_payment(origin, value: T::Balance) -> Result;
+		fn play(origin) -> Result {
+			let sender = ensure_signed(origin)?;
+			let payment = Self::payment().ok_or("Must have payment amount set")?;
+
+			<balances::Module<T>>::decrease_free_balance(&sender, payment)?;
+
+			if (<system::Module<T>>::random_seed(), &sender)
+				.using_encoded(<T as system::Trait>::Hashing::hash)
+				.using_encoded(|e| e[0] < 128)
+			{
+				<balances::Module<T>>::increase_free_balance_creating(&sender, <Pot<T>>::take());
+			}
+
+			<Pot<T>>::mutate(|pot| *pot += payment);
+
+			Ok(())
+		}
+		fn set_payment(_origin, value: T::Balance) -> Result {
+			if Self::payment().is_none() {
+				<Payment<T>>::put(value);
+				<Pot<T>>::put(value);
+			}
+			Ok(())
+		}
 	}
 }
 
@@ -19,33 +41,5 @@ decl_storage! {
 	trait Store for Module<T: Trait> as Demo {
 		Payment get(payment) config(): Option<T::Balance>;
 		Pot get(pot): T::Balance;
-	}
-}
-
-impl<T: Trait> Module<T> {
-	fn play(origin: T::Origin) -> Result {
-		let sender = ensure_signed(origin)?;
-		let payment = Self::payment().ok_or("Must have payment amount set")?;
-
-		<balances::Module<T>>::decrease_free_balance(&sender, payment)?;
-
-		if (<system::Module<T>>::random_seed(), &sender)
-			.using_encoded(<T as system::Trait>::Hashing::hash)
-			.using_encoded(|e| e[0] < 128)
-		{
-			<balances::Module<T>>::increase_free_balance_creating(&sender, <Pot<T>>::take());
-		}
-
-		<Pot<T>>::mutate(|pot| *pot += payment);
-
-		Ok(())
-	}
-
-	fn set_payment(_: T::Origin, value: T::Balance) -> Result {
-		if Self::payment().is_none() {
-			<Payment<T>>::put(value);
-			<Pot<T>>::put(value);
-		}
-		Ok(())
 	}
 }
