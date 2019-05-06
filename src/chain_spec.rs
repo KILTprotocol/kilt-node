e5be8baca3077894beec519ee074b8d1e887cf84
Benjamin Kolb <benjamin.kolb@sprylab.com>
2019-05-06 16:05:13 +0200

    feat: adjust initial chain spec

--- src/chain_spec.rs
+++ src/chain_spec.rs
@@ -105,9 +105,9 @@ fn testnet_genesis(initial_authorities: Vec<AuthorityId>, endowed_accounts: Vec<
 			ids: endowed_accounts.clone(),
 		}),
 		balances: Some(BalancesConfig {
-			transaction_base_fee: 1,
+			transaction_base_fee: 1000000,
 			transaction_byte_fee: 0,
-			existential_deposit: 500,
+			existential_deposit: 1000000,
 			transfer_fee: 0,
 			creation_fee: 0,
 			balances: endowed_accounts.iter().cloned().map(|k|(k, 1 << 60)).collect(),
