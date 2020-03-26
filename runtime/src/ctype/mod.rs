5d5de1e8448f35e22fb1fe107efe634a64131032
Albrecht <14820950+weichweich@users.noreply.github.com>
2020-03-26 10:42:38 +0100

    wip: fix deposit_event

--- runtime/src/ctype/mod.rs
+++ runtime/src/ctype/mod.rs
@@ -46,7 +46,7 @@ decl_module! {
 	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
 
 		/// Deposit events
-		fn deposit_event<T>() = default;
+		fn deposit_event() = default;
 
 		/// Adds a CTYPE on chain, where
 		/// origin - the origin of the transaction
