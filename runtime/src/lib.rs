60a7e7450cbfe946b6ab743645e1eda44e857aeb
Albrecht <14820950+weichweich@users.noreply.github.com>
2020-04-15 07:48:12 +0200

    feat: increase runtime versions

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -113,9 +113,9 @@ pub mod opaque {
 pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("mashnet-node"),
 	impl_name: create_runtime_str!("mashnet-node"),
-	authoring_version: 3,
+	authoring_version: 4,
 	spec_version: 4,
-	impl_version: 4,
+	impl_version: 5,
 	apis: RUNTIME_API_VERSIONS,
 };
 
