8fc7942370e65834df3cb5fab67016fc6193ff3f
wischli <w.freude@icloud.com>
2020-06-05 10:57:32 +0200

    refactor: switch maximum blockweight

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -140,7 +140,8 @@ pub fn native_version() -> NativeVersion {
 
 parameter_types! {
 	pub const BlockHashCount: BlockNumber = 250;
-	pub const MaximumBlockWeight: Weight = 1_000_000_000;
+	/// We allow for 2 seconds of compute with a 6 second average block time.
+	pub const MaximumBlockWeight: Weight = 2 * WEIGHT_PER_SECOND;
 	pub const AvailableBlockRatio: Perbill = Perbill::from_percent(75);
 	/// Assume 10% of weight for average on_initialize calls.
 	pub const MaximumExtrinsicWeight: Weight = AvailableBlockRatio::get()
