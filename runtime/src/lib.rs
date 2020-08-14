a048f208fa406964b0234712c896bac2d7fea474
William Freudenberger <william@kilt.io>
2020-08-14 15:38:21 +0200

    refactor: formatting

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -402,7 +402,7 @@ impl_runtime_apis! {
 		}
 	}
 
-	impl system_rpc_runtime_api::AccountNonceApi<Block, AccountId, Index> for Runtime {
+	impl frame_system_rpc_runtime_api::AccountNonceApi<Block, AccountId, Index> for Runtime {
 		fn account_nonce(account: AccountId) -> Index {
 			frame_system::Module::<Runtime>::account_nonce(&account)
 		}
