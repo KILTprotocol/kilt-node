ec727312f391b5a04c15c03deba5d2ed0add30be
weichweich <14820950+weichweich@users.noreply.github.com>
2021-02-04 15:46:51 +0100

    include chainspec in binary

--- nodes/parachain/src/chain_spec.rs
+++ nodes/parachain/src/chain_spec.rs
@@ -29,8 +29,6 @@ use serde::{Deserialize, Serialize};
 use sp_core::{sr25519, Pair, Public};
 use sp_runtime::traits::{IdentifyAccount, Verify};
 
-use hex_literal::hex;
-
 /// Specialized `ChainSpec` for the normal parachain runtime.
 pub type ChainSpec = sc_service::GenericChainSpec<GenesisConfig, Extensions>;
 
@@ -118,64 +116,12 @@ pub fn get_chain_spec(id: ParaId) -> Result<ChainSpec, String> {
 	))
 }
 
-pub fn staging_test_net(id: ParaId) -> Result<ChainSpec, String> {
-	let properties = get_properties("KILT", 18, 38);
-	let wasm = WASM_BINARY.ok_or("No WASM")?;
-
-	Ok(ChainSpec::from_genesis(
-		"KILT Collator Staging Testnet",
-		"kilt_parachain_staging_testnet",
-		ChainType::Live,
-		move || {
-			testnet_genesis(
-				wasm,
-				hex!["d206033ba2eadf615c510f2c11f32d931b27442e5cfb64884afa2241dfa66e70"].into(),
-				vec![
-					hex!["d206033ba2eadf615c510f2c11f32d931b27442e5cfb64884afa2241dfa66e70"].into(),
-					hex!["b67fe6413ffe5cf91ae38a6475c37deea70a25c6c86b3dd17bb82d09efd9b350"].into(),
-				],
-				id,
-			)
-		},
-		Vec::new(),
-		None,
-		None,
-		Some(properties),
-		Extensions {
-			relay_chain: "rococo_staging_testnet".into(),
-			para_id: id.into(),
-		},
-	))
+pub fn staging_test_net() -> Result<ChainSpec, String> {
+	ChainSpec::from_json_bytes(&include_bytes!("../res/staging.json")[..])
 }
 
-pub fn rococo_net(id: ParaId) -> Result<ChainSpec, String> {
-	let properties = get_properties("KILT", 18, 38);
-	let wasm = WASM_BINARY.ok_or("No WASM")?;
-
-	Ok(ChainSpec::from_genesis(
-		"KILT Collator Rococo",
-		"kilt_parachain_rococo",
-		ChainType::Live,
-		move || {
-			testnet_genesis(
-				wasm,
-				hex!["da878f0b4cb36a3024015d47b9b3cc3be1573d8a744922e4ed9ab2665f469306"].into(),
-				vec![
-					hex!["da878f0b4cb36a3024015d47b9b3cc3be1573d8a744922e4ed9ab2665f469306"].into(),
-					hex!["f022c0f9c929bec1a5069d8806feab127f2fc4fc31d3a5d8db56d00aeec0ff3c"].into(),
-				],
-				id,
-			)
-		},
-		Vec::new(),
-		None,
-		None,
-		Some(properties),
-		Extensions {
-			relay_chain: "rococo_v1".into(),
-			para_id: id.into(),
-		},
-	))
+pub fn rococo_net() -> Result<ChainSpec, String> {
+	ChainSpec::from_json_bytes(&include_bytes!("../res/rococo.json")[..])
 }
 
 fn testnet_genesis(
--- nodes/parachain/src/command.rs
+++ nodes/parachain/src/command.rs
@@ -42,8 +42,9 @@ fn load_spec(
 	para_id: ParaId,
 ) -> std::result::Result<Box<dyn sc_service::ChainSpec>, String> {
 	match id {
-		"staging" => Ok(Box::new(chain_spec::staging_test_net(para_id)?)),
-		"rococo" => Ok(Box::new(chain_spec::rococo_net(para_id)?)),
+		"staging" => Ok(Box::new(chain_spec::staging_test_net()?)),
+		"rococo" => Ok(Box::new(chain_spec::rococo_net()?)),
+		"dev" => Ok(Box::new(chain_spec::get_chain_spec(para_id)?)),
 		"" => Ok(Box::new(chain_spec::get_chain_spec(para_id)?)),
 		path => Ok(Box::new(chain_spec::ChainSpec::from_json_file(
 			path.into(),
