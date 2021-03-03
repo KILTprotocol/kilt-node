fa3c31911e4136ab84a5e0a3bbcda0e6be77314b
weichweich <14820950+weichweich@users.noreply.github.com>
2021-03-03 17:01:33 +0100

    add "forked" pallets

---
+++ pallets/pallet-aura-2.0.0-rc5/src/mock.rs
@@ -0,0 +1,95 @@
+// This file is part of Substrate.
+
+// Copyright (C) 2018-2020 Parity Technologies (UK) Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! Test utilities
+
+#![cfg(test)]
+
+use crate::{Trait, Module, GenesisConfig};
+use sp_consensus_aura::ed25519::AuthorityId;
+use sp_runtime::{
+	traits::IdentityLookup, Perbill,
+	testing::{Header, UintAuthorityId},
+};
+use frame_support::{impl_outer_origin, parameter_types, weights::Weight};
+use sp_io;
+use sp_core::H256;
+
+impl_outer_origin!{
+	pub enum Origin for Test where system = frame_system {}
+}
+
+// Workaround for https://github.com/rust-lang/rust/issues/26925 . Remove when sorted.
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct Test;
+
+parameter_types! {
+	pub const BlockHashCount: u64 = 250;
+	pub const MaximumBlockWeight: Weight = 1024;
+	pub const MaximumBlockLength: u32 = 2 * 1024;
+	pub const AvailableBlockRatio: Perbill = Perbill::one();
+	pub const MinimumPeriod: u64 = 1;
+}
+
+impl frame_system::Trait for Test {
+	type BaseCallFilter = ();
+	type Origin = Origin;
+	type Index = u64;
+	type BlockNumber = u64;
+	type Call = ();
+	type Hash = H256;
+	type Hashing = ::sp_runtime::traits::BlakeTwo256;
+	type AccountId = u64;
+	type Lookup = IdentityLookup<Self::AccountId>;
+	type Header = Header;
+	type Event = ();
+	type BlockHashCount = BlockHashCount;
+	type MaximumBlockWeight = MaximumBlockWeight;
+	type DbWeight = ();
+	type BlockExecutionWeight = ();
+	type ExtrinsicBaseWeight = ();
+	type MaximumExtrinsicWeight = MaximumBlockWeight;
+	type AvailableBlockRatio = AvailableBlockRatio;
+	type MaximumBlockLength = MaximumBlockLength;
+	type Version = ();
+	type ModuleToIndex = ();
+	type AccountData = ();
+	type OnNewAccount = ();
+	type OnKilledAccount = ();
+	type SystemWeightInfo = ();
+}
+
+impl pallet_timestamp::Trait for Test {
+	type Moment = u64;
+	type OnTimestampSet = Aura;
+	type MinimumPeriod = MinimumPeriod;
+	type WeightInfo = ();
+}
+
+impl Trait for Test {
+	type AuthorityId = AuthorityId;
+}
+
+pub fn new_test_ext(authorities: Vec<u64>) -> sp_io::TestExternalities {
+	let mut t = frame_system::GenesisConfig::default().build_storage::<Test>().unwrap();
+	GenesisConfig::<Test>{
+		authorities: authorities.into_iter().map(|a| UintAuthorityId(a).to_public_key()).collect(),
+	}.assimilate_storage(&mut t).unwrap();
+	t.into()
+}
+
+pub type Aura = Module<Test>;
