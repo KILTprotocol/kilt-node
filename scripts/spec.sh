93dfbbbd45ecf0a89af46a439bb88813b3e31207
weichweich <14820950+weichweich@users.noreply.github.com>
2021-05-05 10:39:08 +0200

    fix suffix

--- scripts/spec.sh
+++ scripts/spec.sh
@@ -9,10 +9,10 @@ docker run parity/rococo:rococo-v1-0.8.30-943038a8-f14fa75f build-spec --chain r
 cargo run --release -p kilt-parachain --features fast-gov -- build-spec --chain peregrine --disable-default-bootnode >peregrine-kilt.plain.spec
 
 jq -f scripts/peregrine-relay.jq rococo.plain.json >rococo.json
-jq -f scripts/peregrine-kilt.jq peregrine-kilt.plain.spec >peregrine-kilt.spec
+jq -f scripts/peregrine-kilt.jq peregrine-kilt.plain.spec >peregrine-kilt.json
 
 docker run -v $PWD:/data/spec parity/rococo:rococo-v1-0.8.30-943038a8-f14fa75f build-spec --chain /data/spec/rococo.json --raw --disable-default-bootnode >dev-specs/kilt-parachain/peregrine-relay.json
-cargo run --release -p kilt-parachain --features fast-gov -- build-spec --chain peregrine-kilt.spec --disable-default-bootnode >dev-specs/kilt-parachain/peregrine-kilt.json
+cargo run --release -p kilt-parachain --features fast-gov -- build-spec --chain peregrine-kilt.json --disable-default-bootnode >dev-specs/kilt-parachain/peregrine-kilt.json
 
 # ##############################################################################
 # #                                                                            #
@@ -20,10 +20,10 @@ cargo run --release -p kilt-parachain --features fast-gov -- build-spec --chain 
 # #                                                                            #
 # ##############################################################################
 docker run parity/rococo:rococo-v1-0.8.30-943038a8-f14fa75f build-spec --chain rococo-local --disable-default-bootnode >rococo.plain.json
-cargo run --release -p kilt-parachain --features fast-gov -- build-spec --chain staging --disable-default-bootnode >kilt-stage.plain.spec
+cargo run --release -p kilt-parachain --features fast-gov -- build-spec --chain staging --disable-default-bootnode >kilt-stage.plain.json
 
 jq -f scripts/roc-stage-relay.jq rococo.plain.json >rococo.json
-jq -f scripts/roc-stage-kilt.jq kilt-stage.plain.spec >kilt-stage.spec
+jq -f scripts/roc-stage-kilt.jq kilt-stage.plain.spec >kilt-stage.json
 
 docker run -v $PWD:/data/spec parity/rococo:rococo-v1-0.8.30-943038a8-f14fa75f build-spec --chain /data/spec/rococo.json --raw --disable-default-bootnode >dev-specs/kilt-parachain/relay-stage.json
-cargo run --release -p kilt-parachain --features fast-gov -- build-spec --chain kilt-stage.spec --disable-default-bootnode >dev-specs/kilt-parachain/kilt-stage.spec
+cargo run --release -p kilt-parachain --features fast-gov -- build-spec --chain kilt-stage.json --disable-default-bootnode >dev-specs/kilt-parachain/kilt-stage.json
