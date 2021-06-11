023ba0ffa61ecf262e823ff06f2d4b544a8e775d
William Freudenberger <william@kilt.io>
2021-06-11 17:18:30 +0200

    wip: try to get there...

--- runtimes/spiritnet/src/fee.rs
+++ runtimes/spiritnet/src/fee.rs
@@ -16,9 +16,13 @@
 
 // If you feel like getting in touch with us, you can do so at info@botlabs.org
 
-use frame_support::weights::{WeightToFeeCoefficient, WeightToFeeCoefficients, WeightToFeePolynomial};
+use frame_support::{
+	traits::Get,
+	weights::{WeightToFeeCoefficient, WeightToFeeCoefficients, WeightToFeePolynomial},
+};
 use kilt_primitives::{constants::KILT, Balance};
 use pallet_balances::WeightInfo;
+use pallet_transaction_payment::OnChargeTransaction;
 use smallvec::smallvec;
 pub use sp_runtime::Perbill;
 use sp_std::marker::PhantomData;
@@ -37,15 +41,25 @@ use sp_std::marker::PhantomData;
 ///     charged.
 pub struct WeightToFee<T>(PhantomData<T>)
 where
-	T: frame_system::Config;
-impl<T: frame_system::Config> WeightToFeePolynomial for WeightToFee<T> {
+	T: frame_system::Config + pallet_transaction_payment::Config,
+	Balance: From<<<T as pallet_transaction_payment::Config>::OnChargeTransaction as OnChargeTransaction<T>>::Balance>;
+impl<T: frame_system::Config + pallet_transaction_payment::Config> WeightToFeePolynomial for WeightToFee<T>
+where
+	Balance: From<<<T as pallet_transaction_payment::Config>::OnChargeTransaction as OnChargeTransaction<T>>::Balance>,
+{
 	type Balance = Balance;
 	fn polynomial() -> WeightToFeeCoefficients<Self::Balance> {
 		// in Spiritnet, transfer weight is mapped to 0.01 KILT:
-		let p = KILT / 100;
-		let q = Balance::from(crate::weights::pallet_balances::WeightInfo::<T>::transfer());
+		let wanted_fee: Balance = KILT / 100;
+		let per_byte: Balance = Balance::from(<T as pallet_transaction_payment::Config>::TransactionByteFee::get());
+		let tx_byte_fee = Perbill::from_parts(1) * per_byte;
 
-		// f(w) = MILLI_KILT / WeightInfo::transfer() * w
+		let max: Balance = wanted_fee.max(tx_byte_fee);
+		let min: Balance = wanted_fee.min(tx_byte_fee);
+		let p: Balance = max - min;
+		let q: Balance = crate::weights::pallet_balances::WeightInfo::<T>::transfer_keep_alive().into();
+
+		// f(w) = MILLI_KILT / WeightInfo::transfer_keep_alive() * w
 		smallvec![WeightToFeeCoefficient {
 			degree: 1,
 			negative: false,
@@ -57,19 +71,34 @@ impl<T: frame_system::Config> WeightToFeePolynomial for WeightToFee<T> {
 
 #[cfg(test)]
 mod tests {
-	use super::WeightToFee;
-	use frame_support::weights::WeightToFeePolynomial;
-	use kilt_primitives::constants::KILT;
-	use pallet_balances::WeightInfo;
+	use super::*;
+	use crate::{AccountId, Call, Runtime, TransactionPayment};
+	use codec::Encode;
+	use sp_runtime::traits::Zero;
 
 	#[test]
-	fn transaction_fee_is_correct_runtime() {
-		assert_eq!(
-			WeightToFee::<crate::Runtime>::calc(
-				&crate::weights::pallet_balances::WeightInfo::<crate::Runtime>::transfer()
-			),
-			KILT / 100
-		);
+	fn transaction_fee_is_correct() {
+		let storage = frame_system::GenesisConfig::default()
+			.build_storage::<Runtime>()
+			.unwrap();
+		let mut ext = sp_io::TestExternalities::new(storage);
+		ext.execute_with(|| {
+			let tx = Call::Balances(pallet_balances::Call::transfer_keep_alive(
+				AccountId::default().into(),
+				Balance::zero(),
+			));
+
+			let info = TransactionPayment::query_fee_details(tx.clone(), tx.encode().len() as u32);
+			let incl_fee = info.inclusion_fee.unwrap();
+			assert_eq!(
+				incl_fee.base_fee + incl_fee.len_fee + incl_fee.adjusted_weight_fee,
+				KILT / 100,
+				"base fee: {:?} byte fee: {:?} adjusted fee {:?}",
+				incl_fee.base_fee,
+				incl_fee.len_fee,
+				incl_fee.adjusted_weight_fee
+			);
+		})
 	}
 
 	// TODO: Add test for full block weight once attestation weights have been
