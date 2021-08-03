974ba36fb5ab88675abdf11f52cbf1297453ae6b
Antonio <antonio@kilt.io>
2021-08-03 16:37:56 +0200

    feat: delegation pallet storage refactor (#226)
    * feat: initial commit
    
    * feat: pallet compiling
    
    * wip: fixing test cases
    
    * wip: refactoring test cases
    
    * test: unit tests passing again
    
    * benchmark tests passing
    
    * test: attestation unit tests passing
    
    * chore: clippy fixes
    
    * chore: fmt fixes
    
    * bench: re-execute benchmarks for delegation and attestation + temporarily remove benchmarks from peregrine runtime
    
    * fix: adjust pre-condition for an attestation unit test
    
    * wip: add initial support for delegation storage migrations
    
    * delegation storage migrator compiling
    
    * wip: storage migrator working, now writing unit tests for it
    
    * test: unit tests passing
    
    * chore: clippy hints
    
    * chore: further clippy fixes
    
    * chore: fmt
    
    * wip: adjusting last things before possible to review
    
    * chore: clippy + fmt
    
    * fix: address part of PR review comments
    
    * fix: more comments from PR review
    
    * wip: change vector of versions to enum
    
    * test: unit tests refactor for the new storage migrator version
    
    * chore: refactor
    
    * test: unit tests passing again
    
    * chore: move deprecated type to deprecated.rs
    
    * chore: refactor import/export
    
    * doc: refine documentation
    
    * chore: fmt + clippy
    
    * test: add TODO to improve post_migration tests
    
    * feat: remove test feature from traits
    
    * test: add checks during migration in try-runtime
    
    * bench: add spiritnet benchmarks for delegation and attestation
    
    * feat: additional logs for count of migrated nodes
    
    * chore: fmt
    
    * chore: removed useless file
    
    * chore: bump runtime version
    
    * fix: PR comments
    
    * fix: re-introduce root_id in delegation creation hash
    
    * chore: capitalize storage version enum
    
    * chore: move migrations in their own module
    
    * chore: move migrations.rs out of the migrations folder
    
    * fix: add check for parent revocation status when creating a delegation
    
    * chore: fmt
    
    * feat: remove revoke_hierarchy extrinsic
    
    * bench: add peregrine benchmarks
    
    * bench: re-run delegation and attestation default benchmarks
    
    * chore: fmt

---
+++ pallets/delegation/src/migrations.rs
@@ -0,0 +1,223 @@
+// KILT Blockchain – https://botlabs.org
+// Copyright (C) 2019-2021 BOTLabs GmbH
+
+// The KILT Blockchain is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// The KILT Blockchain is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+// If you feel like getting in touch with us, you can do so at info@botlabs.org
+
+use codec::{Decode, Encode};
+use sp_std::marker::PhantomData;
+
+use crate::*;
+
+mod v1;
+
+/// A trait that allows version migrators to access the underlying pallet's
+/// context, e.g., its Config trait.
+///
+/// In this way, the migrator can access the pallet's storage and the pallet's
+/// types directly.
+pub trait VersionMigratorTrait<T> {
+	#[cfg(feature = "try-runtime")]
+	fn pre_migrate(&self) -> Result<(), &str>;
+	fn migrate(&self) -> Weight;
+	#[cfg(feature = "try-runtime")]
+	fn post_migrate(&self) -> Result<(), &str>;
+}
+
+/// Storage version of the delegation pallet.
+#[derive(Copy, Clone, Encode, Eq, Decode, Ord, PartialEq, PartialOrd)]
+pub enum DelegationStorageVersion {
+	V1,
+	V2,
+}
+
+#[cfg(feature = "try-runtime")]
+impl DelegationStorageVersion {
+	/// The latest storage version.
+	fn latest() -> Self {
+		Self::V2
+	}
+}
+
+// All nodes will default to this, which is not bad, as in case the "real"
+// version is a later one (i.e. the node has been started with already the
+// latest version), the migration will simply do nothing as there's nothing in
+// the old storage entries to migrate from.
+//
+// It might get updated in the future when we know that no node is running this
+// old version anymore.
+impl Default for DelegationStorageVersion {
+	fn default() -> Self {
+		Self::V1
+	}
+}
+
+impl<T: Config> VersionMigratorTrait<T> for DelegationStorageVersion {
+	// It runs the right pre_migrate logic depending on the current storage version.
+	#[cfg(feature = "try-runtime")]
+	fn pre_migrate(&self) -> Result<(), &str> {
+		match *self {
+			Self::V1 => v1::pre_migrate::<T>(),
+			Self::V2 => Err("Already latest v2 version."),
+		}
+	}
+
+	// It runs the righ migration logic depending on the current storage version.
+	fn migrate(&self) -> Weight {
+		match *self {
+			Self::V1 => v1::migrate::<T>(),
+			Self::V2 => 0u64,
+		}
+	}
+
+	// It runs the right post_migrate logic depending on the current storage
+	// version.
+	#[cfg(feature = "try-runtime")]
+	fn post_migrate(&self) -> Result<(), &str> {
+		match *self {
+			Self::V1 => v1::post_migrate::<T>(),
+			Self::V2 => Err("Migration from v2 should have never happened in the first place."),
+		}
+	}
+}
+
+/// The delegation pallet's storage migrator, which handles all version
+/// migrations in a sequential fashion.
+///
+/// If a node has missed on more than one upgrade, the migrator will apply the
+/// needed migrations one after the other. Otherwise, if no migration is needed,
+/// the migrator will simply not do anything.
+pub struct DelegationStorageMigrator<T>(PhantomData<T>);
+
+impl<T: Config> DelegationStorageMigrator<T> {
+	// Contains the migration sequence logic.
+	fn get_next_storage_version(current: DelegationStorageVersion) -> Option<DelegationStorageVersion> {
+		// If the version current deployed is at least v1, there is no more migrations
+		// to run (other than the one from v1).
+		match current {
+			DelegationStorageVersion::V1 => None,
+			DelegationStorageVersion::V2 => None,
+		}
+	}
+
+	/// Checks whether the latest storage version deployed is lower than the
+	/// latest possible.
+	#[cfg(feature = "try-runtime")]
+	pub(crate) fn pre_migrate() -> Result<(), &'static str> {
+		ensure!(
+			StorageVersion::<T>::get() < DelegationStorageVersion::latest(),
+			"Already the latest storage version."
+		);
+
+		// Don't need to check for any other pre_migrate, as in try-runtime it is also
+		// called in the migrate() function. Same applies for post_migrate checks for
+		// each version migrator.
+
+		Ok(())
+	}
+
+	/// Applies all the needed migrations from the currently deployed version to
+	/// the latest possible, one after the other.
+	///
+	/// It returns the total weight consumed by ALL the migrations applied.
+	pub(crate) fn migrate() -> Weight {
+		let mut current_version: Option<DelegationStorageVersion> = Some(StorageVersion::<T>::get());
+		// Weight for StorageVersion::get().
+		let mut total_weight = T::DbWeight::get().reads(1);
+
+		while let Some(ver) = current_version {
+			// If any of the needed migrations pre-checks fail, the whole chain panics
+			// (during tests).
+			#[cfg(feature = "try-runtime")]
+			if let Err(err) = <DelegationStorageVersion as VersionMigratorTrait<T>>::pre_migrate(&ver) {
+				panic!("{:?}", err);
+			}
+			let consumed_weight = <DelegationStorageVersion as VersionMigratorTrait<T>>::migrate(&ver);
+			total_weight = total_weight.saturating_add(consumed_weight);
+			// If any of the needed migrations post-checks fail, the whole chain panics
+			// (during tests).
+			#[cfg(feature = "try-runtime")]
+			if let Err(err) = <DelegationStorageVersion as VersionMigratorTrait<T>>::post_migrate(&ver) {
+				panic!("{:?}", err);
+			}
+			// If more migrations should be applied, current_version will not be None.
+			current_version = Self::get_next_storage_version(ver);
+		}
+
+		total_weight
+	}
+
+	/// Checks whether the storage version after all the needed migrations match
+	/// the latest one.
+	#[cfg(feature = "try-runtime")]
+	pub(crate) fn post_migrate() -> Result<(), &'static str> {
+		ensure!(
+			StorageVersion::<T>::get() == DelegationStorageVersion::latest(),
+			"Not updated to the latest version."
+		);
+
+		Ok(())
+	}
+}
+
+// Tests for the entire storage migrator.
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	use crate::mock::Test as TestRuntime;
+
+	#[test]
+	fn ok_from_v1_migration() {
+		let mut ext = mock::ExtBuilder::default()
+			.with_storage_version(DelegationStorageVersion::V1)
+			.build(None);
+		ext.execute_with(|| {
+			#[cfg(feature = "try-runtime")]
+			assert!(
+				DelegationStorageMigrator::<TestRuntime>::pre_migrate().is_ok(),
+				"Storage pre-migrate from v1 should not fail."
+			);
+
+			DelegationStorageMigrator::<TestRuntime>::migrate();
+
+			#[cfg(feature = "try-runtime")]
+			assert!(
+				DelegationStorageMigrator::<TestRuntime>::post_migrate().is_ok(),
+				"Storage post-migrate from v1 should not fail."
+			);
+		});
+	}
+
+	#[test]
+	fn ok_from_default_migration() {
+		let mut ext = mock::ExtBuilder::default().build(None);
+		ext.execute_with(|| {
+			#[cfg(feature = "try-runtime")]
+			assert!(
+				DelegationStorageMigrator::<TestRuntime>::pre_migrate().is_ok(),
+				"Storage pre-migrate from default version should not fail."
+			);
+
+			DelegationStorageMigrator::<TestRuntime>::migrate();
+
+			#[cfg(feature = "try-runtime")]
+			assert!(
+				DelegationStorageMigrator::<TestRuntime>::post_migrate().is_ok(),
+				"Storage post-migrate from default version should not fail."
+			);
+		});
+	}
+}
