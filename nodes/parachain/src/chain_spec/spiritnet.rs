a9eb040ec9f2511874a02ddf7b1e040e30f654e4
weichweich <14820950+weichweich@users.noreply.github.com>
2021-08-13 14:53:17 +0200

    refactor: rename files

--- nodes/parachain/src/chain_spec/spiritnet.rs
+++ nodes/parachain/src/chain_spec/spiritnet.rs
@@ -295,12 +295,12 @@ fn testnet_genesis(
 	type LockingPeriod = BlockNumber;
 
 	// vesting and locks as initially designed
-	let airdrop_accounts_json = &include_bytes!("../../res/genesis/genesis-accounts.json")[..];
+	let airdrop_accounts_json = &include_bytes!("../../res/genesis/claimable-accounts.json")[..];
 	let airdrop_accounts: Vec<(AccountId, Balance, VestingPeriod, LockingPeriod)> =
 		serde_json::from_slice(airdrop_accounts_json).expect("The file genesis_accounts.json exists and is valid; qed");
 
 	// botlabs account should not be migrated but some have vesting
-	let botlabs_accounts_json = &include_bytes!("../../res/genesis/botlabs-accounts.json")[..];
+	let botlabs_accounts_json = &include_bytes!("../../res/genesis/owned-accounts.json")[..];
 	let botlabs_accounts: Vec<(AccountId, Balance, VestingPeriod, LockingPeriod)> =
 		serde_json::from_slice(botlabs_accounts_json).expect("The file botlabs_accounts.json exists and is valid; qed");
 
