16222756a584c397065942445536912b80231795
weichweich <14820950+weichweich@users.noreply.github.com>
2021-08-18 09:03:05 +0200

    tests: increase call size limit

--- runtimes/peregrine/src/tests.rs
+++ runtimes/peregrine/src/tests.rs
@@ -21,8 +21,8 @@ use super::Call;
 #[test]
 fn call_size() {
 	assert!(
-		core::mem::size_of::<Call>() <= 230,
-		"size of Call is {:?} bytes which is more than 230 bytes: some calls have too big arguments, use Box to reduce the size of Call.
+		core::mem::size_of::<Call>() <= 240,
+		"size of Call is {:?} bytes which is more than 240 bytes: some calls have too big arguments, use Box to reduce the size of Call.
 		If the limit is too strong, maybe consider increase the limit to 300.",
 		core::mem::size_of::<Call>(),
 	);
