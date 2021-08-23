52771ba2f53e10284c0ffb26a05aac22455e2f33
weichweich <14820950+weichweich@users.noreply.github.com>
2021-08-23 13:32:38 +0200

    increase runtime version

--- runtimes/peregrine/src/lib.rs
+++ runtimes/peregrine/src/lib.rs
@@ -122,7 +122,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("mashnet-node"),
 	impl_name: create_runtime_str!("mashnet-node"),
 	authoring_version: 4,
-	spec_version: 20,
+	spec_version: 21,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,
