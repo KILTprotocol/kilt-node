5780bb07fe7111ef6841c5703e009535798f5c41
Antonio Antonino <antonio@kilt.io>
2021-10-20 09:44:12 +0200

    chore: bump runtime version

--- runtimes/peregrine/src/lib.rs
+++ runtimes/peregrine/src/lib.rs
@@ -102,7 +102,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("mashnet-node"),
 	impl_name: create_runtime_str!("mashnet-node"),
 	authoring_version: 4,
-	spec_version: 2700,
+	spec_version: 2701,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,
