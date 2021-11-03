d27d0629b823bff0893830b100a75793239292c0
weichweich <14820950+weichweich@users.noreply.github.com>
2021-11-03 08:42:29 +0100

    Version 1.1.1

--- runtimes/spiritnet/src/lib.rs
+++ runtimes/spiritnet/src/lib.rs
@@ -103,7 +103,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("kilt-spiritnet"),
 	impl_name: create_runtime_str!("kilt-spiritnet"),
 	authoring_version: 1,
-	spec_version: 10101,
+	spec_version: 10110,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
