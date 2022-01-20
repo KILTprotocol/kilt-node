ad7861932530c42cb1b8f340b76509946071985d
William Freudenberger <william@kilt.io>
2022-01-20 10:56:55 +0100

    feat: add kilt-rococo spec (#313)
    * feat: add rococo spec
    
    * chore: update rilt spec
    
    * feat: add build in rilt spec
    
    * fix: remove duplicate rilt spec

--- nodes/parachain/src/chain_spec/spiritnet.rs
+++ nodes/parachain/src/chain_spec/spiritnet.rs
@@ -168,7 +168,7 @@ pub fn get_chain_spec_wilt() -> Result<ChainSpec, String> {
 				.parse()
 				.expect("bootnode address is formatted correctly; qed"),
 		],
-		Some(TelemetryEndpoints::new(vec![(TELEMETRY_URL.to_string(), 0)]).expect("KILT telemetry url is valid; qed")),
+		Some(TelemetryEndpoints::new(vec![(TELEMETRY_URL.to_string(), 0)]).expect("WILT telemetry url is valid; qed")),
 		None,
 		Some(properties),
 		Extensions {
@@ -176,6 +176,65 @@ pub fn get_chain_spec_wilt() -> Result<ChainSpec, String> {
 			para_id: id.into(),
 		},
 	))
+}
+
+const RILT_COL_ACC_1: [u8; 32] = hex!["6a5c355bca369a54c334542fd91cf70822be92f215a1049ceb04f36baba9b87b"];
+const RILT_COL_SESSION_1: [u8; 32] = hex!["66c4ca0710c2c8a92504f281d992000508ce255543016545014cf0bfbbe71429"];
+const RILT_COL_ACC_2: [u8; 32] = hex!["768538a941d1e4730c31830ab85a54ff34aaaad1f81bdd246db11802a57a5412"];
+const RILT_COL_SESSION_2: [u8; 32] = hex!["7cff6c7a53c4630a0a35f8793a04b663681575bbfa43dbe5848b220bc4bd1963"];
+const RILT_TRANS_ACC: [u8; 32] = hex!["6e016ca65cd213156c075da95c132bd1917762de0026ee539c720999aded3216"];
+
+pub fn get_chain_spec_rilt() -> Result<ChainSpec, String> {
+	let properties = get_properties("RILT", 15, 38);
+	let wasm = WASM_BINARY.ok_or("No WASM")?;
+	let id: ParaId = 2015.into();
+
+	Ok(ChainSpec::from_genesis(
+		"RILT",
+		"kilt_rococo",
+		ChainType::Live,
+		move || {
+			testnet_genesis(
+				wasm,
+				vec![
+					(RILT_COL_ACC_1.into(), None, 200_000 * KILT),
+					(RILT_COL_ACC_2.into(), None, 200_000 * KILT),
+				],
+				kilt_inflation_config(),
+				MAX_COLLATOR_STAKE,
+				vec![
+					(RILT_COL_ACC_1.into(), RILT_COL_SESSION_1.unchecked_into()),
+					(RILT_COL_ACC_2.into(), RILT_COL_SESSION_2.unchecked_into()),
+				],
+				vec![
+					(RILT_COL_ACC_1.into(), 1_000_000 * KILT),
+					(RILT_COL_ACC_2.into(), 1_000_000 * KILT),
+					(RILT_TRANS_ACC.into(), 10_000 * KILT),
+				],
+				RILT_TRANS_ACC.into(),
+				id,
+			)
+		},
+		vec![
+			"/dns4/bootnode.kilt.io/tcp/30365/p2p/12D3KooWS2h3rxqEC9bzrFNKVgrT1iaGz2UAWA1jVG1EB6dEoeJm"
+				.parse()
+				.expect("bootnode address is formatted correctly; qed"),
+			"/dns4/bootnode.kilt.io/tcp/30366/p2p/12D3KooWMSF7Vefmpf67iGMkPrUgvXw38HoxaLmTNpYGYikFS7DZ"
+				.parse()
+				.expect("bootnode address is formatted correctly; qed"),
+		],
+		Some(TelemetryEndpoints::new(vec![(TELEMETRY_URL.to_string(), 0)]).expect("RILT telemetry url is valid; qed")),
+		None,
+		Some(properties),
+		Extensions {
+			relay_chain: "rococo".into(),
+			para_id: id.into(),
+		},
+	))
+}
+
+pub fn load_rilt_spec() -> Result<ChainSpec, String> {
+	ChainSpec::from_json_bytes(&include_bytes!("../../res/rilt.json")[..])
 }
 
 pub fn load_spiritnet_spec() -> Result<ChainSpec, String> {
