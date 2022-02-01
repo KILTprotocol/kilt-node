23f3139317fc6ff8ee9c09c3cda6c35d62a964bc
William Freudenberger <william@kilt.io>
2022-02-01 11:58:59 +0100

    fix: preimage weights

--- runtimes/peregrine/src/weights/frame_system.rs
+++ runtimes/peregrine/src/weights/frame_system.rs
@@ -60,11 +60,6 @@ impl<T: frame_system::Config> frame_system::WeightInfo for WeightInfo<T> {
 	fn set_heap_pages() -> Weight {
 		(2_505_000_u64).saturating_add(T::DbWeight::get().writes(1_u64))
 	}
-	fn set_changes_trie_config() -> Weight {
-		(10_569_000_u64)
-			.saturating_add(T::DbWeight::get().reads(1_u64))
-			.saturating_add(T::DbWeight::get().writes(2_u64))
-	}
 	fn set_storage(i: u32) -> Weight {
 		(0_u64)
 			// Standard Error: 1_000
