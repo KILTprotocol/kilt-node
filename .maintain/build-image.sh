1b65854c84e632b2fedd2b871b97fec90baa135a
weichweich <albrecht@kilt.io>
2022-02-23 10:06:25 +0100

    rename to local

--- .maintain/build-image.sh
+++ .maintain/build-image.sh
@@ -3,5 +3,5 @@
 target_tag=$1
 
 # build parachain image and standalone image
-docker build --cache-from $AWS_REGISTRY/kilt-parachain/collator:$target_tag --build-arg NODE_TYPE=kilt-parachain -t kiltprotocol/kilt-node:$target_tag .
-docker build --cache-from $AWS_REGISTRY/kilt/prototype-chain:$target_tag --build-arg NODE_TYPE=mashnet-node -t kiltprotocol/mashnet-node:$target_tag .
+docker build --cache-from $AWS_REGISTRY/kilt-parachain/collator:$target_tag --build-arg NODE_TYPE=kilt-parachain -t local/kilt-node:$target_tag .
+docker build --cache-from $AWS_REGISTRY/kilt/prototype-chain:$target_tag --build-arg NODE_TYPE=mashnet-node -t local/mashnet-node:$target_tag .
