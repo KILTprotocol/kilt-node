9cd44d2c810cb27256b6fbecc4c6859ab7f01420
Antonio Antonino <antonio@kilt.io>
2022-03-21 14:23:37 +0100

    bench: benchmark compiling

--- runtimes/peregrine/src/migrations/mod.rs
+++ runtimes/peregrine/src/migrations/mod.rs
@@ -1,5 +1,5 @@
 use crate::{DidLookup, Runtime, Weight};
-use frame_support::traits::{OnRuntimeUpgrade, StorageVersion as NewStorageVersion};
+use frame_support::traits::OnRuntimeUpgrade;
 use sp_std::marker::PhantomData;
 
 #[cfg(feature = "try-runtime")]
@@ -12,20 +12,22 @@ impl OnRuntimeUpgrade for LookupReverseIndexMigration<Runtime> {
 	#[cfg(feature = "try-runtime")]
 	fn pre_upgrade() -> Result<(), &'static str> {
 		assert_ne!(DidLookup::on_chain_storage_version(), DidLookup::current_storage_version());
-		assert_eq!(DidLookup::<Runtime>::connected_accounts().iter().count(), 0);
+		assert_eq!(pallet_did_lookup::ConnectedAccounts::<Runtime>::iter().count(), 0);
+
+		Ok(())
 	}
 
     fn on_runtime_upgrade() -> frame_support::weights::Weight {
 		// Account for the new storage version written below.
-		let mut total_weight: Weight = Runtime::DbWeight::get().write(1);
+		let mut total_weight: Weight = <Runtime as frame_system::Config>::DbWeight::get().writes(1);
 
 		pallet_did_lookup::ConnectedDids::<Runtime>::iter().for_each(|(account, record)| {
 			pallet_did_lookup::ConnectedAccounts::<Runtime>::insert(record.did, account, ());
 			// One read for the `ConnectedDids` entry, one write for the new `ConnectedAccounts` entry.
-			total_weight = total_weight.saturating_add(Runtime::DbWeight::get().reads_writes(1, 1))
+			total_weight = total_weight.saturating_add(<Runtime as frame_system::Config>::DbWeight::get().reads_writes(1, 1))
 		});
 
-		NewStorageVersion::new(DidLookup::current_storage_version()).put::<DidLookup>();
+		DidLookup::current_storage_version().put::<DidLookup>();
 
 		total_weight
 	}
@@ -40,7 +42,10 @@ impl OnRuntimeUpgrade for LookupReverseIndexMigration<Runtime> {
 		});
 		// Verify Account -> DID integrity.
 		pallet_did_lookup::ConnectedAccounts::<Runtime>::iter().for_each(|(did, account, _)| {
-			assert!(pallet_did_lookup::ConnectedDids::<Runtime>::contains_key(did, account));
+			let entry = pallet_did_lookup::ConnectedDids::<Runtime>::get(account).expect("Should find a record for the given account.");
+			assert_eq!(entry.did, did);
 		});
+
+		Ok(())
 	}
 }
