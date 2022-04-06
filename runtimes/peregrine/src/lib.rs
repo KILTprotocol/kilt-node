4f144aa58f97c22038dda6ae03e50e5a8892f226
Antonio Antonino <antonio@kilt.io>
2022-04-06 13:32:29 +0200

    chore: bump transaction_version

--- runtimes/peregrine/src/lib.rs
+++ runtimes/peregrine/src/lib.rs
@@ -84,7 +84,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_version: 10610,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 2,
+	transaction_version: 3,
 	state_version: 0,
 };
 
