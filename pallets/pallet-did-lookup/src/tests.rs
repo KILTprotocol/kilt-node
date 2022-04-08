31ae90991b9ba8b4b1a3f20f6ee5562ff1919b8b
Antonio <antonio@kilt.io>
2022-04-08 13:25:49 +0200

    fix: account linking signature verification (#354)
    * feat: setup payload bytes wrapping
    
    * fix: pad expected payload with proper <Bytes>...</Bytes>
    
    * chore: fmt
    
    * fix: Vec import
    
    * chore: clippy warning
    
    * chore: bump spec and transaction version
    
    * test: test byte wrapping explicitely
    
    * bench: fix associate_account benchmarks
    
    * chore: replace link with permalink
    
    * bench: updated benchmarks
    
    * fix: pallet lookup benchmarks
    
    * chore: bump crates version

--- pallets/pallet-did-lookup/src/tests.rs
+++ pallets/pallet-did-lookup/src/tests.rs
@@ -82,8 +82,12 @@ fn test_add_association_account() {
 			let pair_alice = sr25519::Pair::from_seed(&*b"Alice                           ");
 			let expire_at: BlockNumber = 500;
 			let account_hash_alice = MultiSigner::from(pair_alice.public()).into_account();
-			let sig_alice_0 = MultiSignature::from(pair_alice.sign(&Encode::encode(&(&DID_00, expire_at))[..]));
-			let sig_alice_1 = MultiSignature::from(pair_alice.sign(&Encode::encode(&(&DID_01, expire_at))[..]));
+			let sig_alice_0 = MultiSignature::from(
+				pair_alice.sign(&[b"<Bytes>", &Encode::encode(&(&DID_00, expire_at))[..], b"</Bytes>"].concat()[..]),
+			);
+			let sig_alice_1 = MultiSignature::from(
+				pair_alice.sign(&[b"<Bytes>", &Encode::encode(&(&DID_01, expire_at))[..], b"</Bytes>"].concat()[..]),
+			);
 
 			// new association. No overwrite
 			assert!(DidLookup::associate_account(
@@ -170,8 +174,9 @@ fn test_add_association_account_invalid_signature() {
 		.execute_with(|| {
 			let pair_alice = sr25519::Pair::from_seed(&*b"Alice                           ");
 			let account_hash_alice = MultiSigner::from(pair_alice.public()).into_account();
-			let sig_alice_0 = MultiSignature::from(pair_alice.sign(&Encode::encode(&0_u64)[..]));
 			let expire_at: BlockNumber = 500;
+			// Try signing only the encoded tuple without the <Bytes>...</Bytes> wrapper
+			let sig_alice_0 = MultiSignature::from(pair_alice.sign(&Encode::encode(&(&DID_01, expire_at))[..]));
 
 			assert_noop!(
 				DidLookup::associate_account(
@@ -193,8 +198,10 @@ fn test_add_association_account_expired() {
 		.execute_with(|| {
 			let pair_alice = sr25519::Pair::from_seed(&*b"Alice                           ");
 			let account_hash_alice = MultiSigner::from(pair_alice.public()).into_account();
-			let sig_alice_0 = MultiSignature::from(pair_alice.sign(&Encode::encode(&0_u64)[..]));
 			let expire_at: BlockNumber = 2;
+			let sig_alice_0 = MultiSignature::from(
+				pair_alice.sign(&[b"<Bytes>", &Encode::encode(&(&DID_01, expire_at))[..], b"</Bytes>"].concat()[..]),
+			);
 			System::set_block_number(3);
 
 			assert_noop!(
--- runtimes/standalone/src/lib.rs
+++ runtimes/standalone/src/lib.rs
@@ -114,10 +114,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("mashnet-node"),
 	impl_name: create_runtime_str!("mashnet-node"),
 	authoring_version: 4,
-	spec_version: 10610,
+	spec_version: 10620,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 2,
+	transaction_version: 3,
 	state_version: 0,
 };
 
