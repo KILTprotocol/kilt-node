c92232de4e9c0f16219dcc196cb662c5a03578e2
weichweich <albrecht@kilt.io>
2022-04-11 15:18:10 +0200

    doc: mention cargo-workspaces

--- scripts/all.fish
+++ scripts/all.fish
@@ -1,3 +1,7 @@
+#!/bin/env fish
+# Check a few feature combinations for all crates.
+# Requires `cargo-workspaces` to be installed.
+
 for features in "--all-features" "--features runtime-benchmarks" "--features try-runtime"
 	for package in (cargo workspaces list)
 		cargo build -p $package (echo $features | string split " ") > /dev/null ^ /dev/null
