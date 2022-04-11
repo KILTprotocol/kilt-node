38018cb131ed10436f6752efa761b30fff8ef3e3
weichweich <albrecht@kilt.io>
2022-04-11 15:38:21 +0200

    check default features

--- scripts/all.fish
+++ scripts/all.fish
@@ -2,7 +2,7 @@
 # Check a few feature combinations for all crates.
 # Requires `cargo-workspaces` to be installed.
 
-for features in "--all-features" "--features runtime-benchmarks" "--features try-runtime"
+for features in "--features default" "--all-features" "--features runtime-benchmarks" "--features try-runtime"
 	for package in (cargo workspaces list)
 		cargo build -p $package (echo $features | string split " ") > /dev/null ^ /dev/null
 		if [ "$status" = "0" ]
