c45dba4aa1071af0f05bbee32a7665b9bd1ee2d0
William Freudenberger <william@kilt.io>
2022-05-02 17:03:11 +0200

    fix: migration

--- runtimes/common/src/migrations.rs
+++ runtimes/common/src/migrations.rs
@@ -18,20 +18,20 @@
 
 use core::marker::PhantomData;
 use frame_support::traits::Get;
-use hex_literal::hex;
 
 pub struct RemoveKiltLaunch<R>(PhantomData<R>);
 impl<R: frame_system::Config> frame_support::traits::OnRuntimeUpgrade for RemoveKiltLaunch<R> {
 	fn on_runtime_upgrade() -> frame_support::weights::Weight {
-		let items = match frame_support::storage::unhashed::kill_prefix(&hex!("37be294ab4b5aa76f1df3f80e7c180ef"), None)
-		{
+		let prefix: [u8; 16] = sp_io::hashing::twox_128(b"KiltLaunch");
+
+		let items = match frame_support::storage::unhashed::kill_prefix(&prefix, Some(6)) {
 			sp_io::KillStorageResult::AllRemoved(n) => {
-				log::info!("🚀 Successfully removed all {} storage items of the launch pallet", n);
+				log::info!("🚀 Successfully removed all {:?} storage items of the launch pallet", n);
 				n
 			}
 			sp_io::KillStorageResult::SomeRemaining(n) => {
 				log::warn!(
-					"🚀  Failed to remove all storage items of the launch pallet, {} are remaining",
+					"🚀  Failed to remove all storage items of the launch pallet, {:?} are remaining",
 					n
 				);
 				n
@@ -42,36 +42,22 @@ impl<R: frame_system::Config> frame_support::traits::OnRuntimeUpgrade for Remove
 
 	#[cfg(feature = "try-runtime")]
 	fn pre_upgrade() -> Result<(), &'static str> {
-		// FIXME: Why does this fail?
-		log::info!(
-			"🚀 Pre check: Launch pallet storage exists {}?",
-			frame_support::storage::migration::have_storage_value(
-				&hex!("37be294ab4b5aa76f1df3f80e7c180ef"),
-				// b"KiltLaunch"
-				// b"TransferAccount",
-				&hex!("73c7528dff85a7339c3d647527b5affb"),
-				&[]
-			)
-		);
+		let prefix: [u8; 16] = sp_io::hashing::twox_128(b"KiltLaunch");
 
-		assert!(frame_support::storage::migration::have_storage_value(
-			&hex!("37be294ab4b5aa76f1df3f80e7c180ef"),
-			// b"KiltLaunch",
-			// b"TransferAccount",
-			&hex!("73c7528dff85a7339c3d647527b5affb"),
-			&[]
-		));
+		assert!(
+			sp_io::storage::next_key(&prefix).map_or(true, |next_key| next_key.starts_with(&prefix)),
+			"🚀 Pre check: Launch pallet storage does not exist!"
+		);
 		Ok(())
 	}
 
 	#[cfg(feature = "try-runtime")]
 	fn post_upgrade() -> Result<(), &'static str> {
-		match frame_support::storage::unhashed::kill_prefix(&hex!("37be294ab4b5aa76f1df3f80e7c180ef"), Some(1)) {
-			sp_io::KillStorageResult::AllRemoved(0) => {
-				log::info!("🚀 Post check: Launch pallet storage successfully removed");
-				Ok(())
-			}
-			_ => Err("🚀 Post check: Launch pallet storage still exists!"),
-		}
+		let prefix: [u8; 16] = sp_io::hashing::twox_128(b"KiltLaunch");
+		assert!(
+			sp_io::storage::next_key(&prefix,).map_or(true, |next_key| !next_key.starts_with(&prefix)),
+			"🚀 Post check: Launch pallet storage still exists!"
+		);
+		Ok(())
 	}
 }
