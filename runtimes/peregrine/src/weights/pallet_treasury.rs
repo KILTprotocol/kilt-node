d8899eccbd056bd72b043722a50c0ff9c72fda36
William Freudenberger <william@kilt.io>
2022-06-07 16:15:27 +0200

    fix: clippy

--- runtimes/peregrine/src/weights/pallet_treasury.rs
+++ runtimes/peregrine/src/weights/pallet_treasury.rs
@@ -50,6 +50,7 @@
 #![cfg_attr(rustfmt, rustfmt_skip)]
 #![allow(unused_parens)]
 #![allow(unused_imports)]
+#![allow(clippy::unnecessary_cast)]
 
 use frame_support::{traits::Get, weights::Weight};
 use sp_std::marker::PhantomData;
