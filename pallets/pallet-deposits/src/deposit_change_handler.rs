074492ea16087a2b6a405f6d83eda0622d03e496
Tino Rusch <tino@kilt.io>
2022-07-27 13:06:47 +0200

    add two comments

--- pallets/pallet-deposits/src/deposit_change_handler.rs
+++ pallets/pallet-deposits/src/deposit_change_handler.rs
@@ -3,13 +3,16 @@ use sp_runtime::DispatchResult;
 
 use crate::{Config, DepositIdOf, AccountIdOf, BalanceOf};
 
+/// DepositChangeHandler is meant to be implemented on the runtime level to allow it to react to the event when a deposit changed
 pub trait DepositChangeHandler<T: Config> {
+    /// on_deposit_paid is called when a deposit was paid.
     fn on_deposit_paid(
         deposit: &DepositIdOf<T>,
         account: &AccountIdOf<T>,
         amount: BalanceOf<T>,
     );
 
+    /// on_deposit_take_announcement is called when a deposit was withdrawn.
     fn on_deposit_taken(
         deposit: &DepositIdOf<T>,
         account: &AccountIdOf<T>,
@@ -26,6 +29,7 @@ impl<T: Config> DepositChangeHandler<T> for () {
     }
 }
 
+// DepositChecker is implemented by the pallet and is meant to be used in other pallets which need deposits to be checked.
 pub trait DepositChecker<T: Config> {
     // check the current total deposit for a given deposit id
     fn check_deposit(deposit_id: &DepositIdOf<T>) -> BalanceOf<T>;
