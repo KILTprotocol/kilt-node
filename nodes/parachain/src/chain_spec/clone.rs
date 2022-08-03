56a871e24773fdbf327c7573939cfd8a92f9d62e
weichweich <albrecht@kilt.io>
2022-08-03 08:01:46 +0200

    clone node

---
+++ nodes/parachain/src/chain_spec/clone.rs
@@ -0,0 +1,248 @@
+// KILT Blockchain – https://botlabs.org
+// Copyright (C) 2019-2022 BOTLabs GmbH
+
+// The KILT Blockchain is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// The KILT Blockchain is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+// If you feel like getting in touch with us, you can do so at info@botlabs.org
+
+//! KILT chain specification
+
+use clone_runtime::{
+	BalancesConfig, CollatorSelectionConfig, CouncilConfig, GenesisConfig, InflationInfo, ParachainInfoConfig,
+	ParachainStakingConfig, PolkadotXcmConfig, SessionConfig, SudoConfig, SystemConfig, TechnicalCommitteeConfig,
+	WASM_BINARY,
+};
+use cumulus_primitives_core::ParaId;
+use hex_literal::hex;
+use runtime_common::{
+	constants::{kilt_inflation_config, staking::MinCollatorStake, KILT, MAX_COLLATOR_STAKE},
+	AccountId, AuthorityId, Balance,
+};
+use sc_service::ChainType;
+use sc_telemetry::TelemetryEndpoints;
+use sp_core::{crypto::UncheckedInto, sr25519};
+
+use crate::chain_spec::{get_account_id_from_seed, get_from_seed, DEFAULT_PARA_ID, TELEMETRY_URL};
+
+use super::{get_properties, Extensions};
+
+const SAFE_XCM_VERSION: u32 = xcm::prelude::XCM_VERSION;
+
+/// Specialized `ChainSpec` for the normal parachain runtime.
+pub type ChainSpec = sc_service::GenericChainSpec<GenesisConfig, Extensions>;
+
+pub fn get_chain_spec_dev() -> Result<ChainSpec, String> {
+	let properties = get_properties("KILT", 15, 38);
+	let wasm = WASM_BINARY.ok_or("No WASM")?;
+
+	Ok(ChainSpec::from_genesis(
+		"KILT clone Develop",
+		"kilt_clone_dev",
+		ChainType::Local,
+		move || {
+			testnet_genesis(
+				wasm,
+				vec![
+					(
+						get_account_id_from_seed::<sr25519::Public>("Alice"),
+						None,
+						2 * MinCollatorStake::get(),
+					),
+					(
+						get_account_id_from_seed::<sr25519::Public>("Bob"),
+						None,
+						2 * MinCollatorStake::get(),
+					),
+				],
+				kilt_inflation_config(),
+				MAX_COLLATOR_STAKE,
+				vec![
+					(
+						get_account_id_from_seed::<sr25519::Public>("Alice"),
+						get_from_seed::<AuthorityId>("Alice"),
+					),
+					(
+						get_account_id_from_seed::<sr25519::Public>("Bob"),
+						get_from_seed::<AuthorityId>("Bob"),
+					),
+				],
+				vec![
+					(get_account_id_from_seed::<sr25519::Public>("Alice"), 10000000 * KILT),
+					(get_account_id_from_seed::<sr25519::Public>("Bob"), 10000000 * KILT),
+					(get_account_id_from_seed::<sr25519::Public>("Charlie"), 10000000 * KILT),
+					(get_account_id_from_seed::<sr25519::Public>("Dave"), 10000000 * KILT),
+					(get_account_id_from_seed::<sr25519::Public>("Eve"), 10000000 * KILT),
+					(get_account_id_from_seed::<sr25519::Public>("Ferdie"), 10000000 * KILT),
+					(
+						get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
+						10000000 * KILT,
+					),
+					(
+						get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
+						10000000 * KILT,
+					),
+					(
+						get_account_id_from_seed::<sr25519::Public>("Charlie//stash"),
+						10000000 * KILT,
+					),
+					(
+						get_account_id_from_seed::<sr25519::Public>("Dave//stash"),
+						10000000 * KILT,
+					),
+					(
+						get_account_id_from_seed::<sr25519::Public>("Eve//stash"),
+						10000000 * KILT,
+					),
+					(
+						get_account_id_from_seed::<sr25519::Public>("Ferdie//stash"),
+						10000000 * KILT,
+					),
+				],
+				get_account_id_from_seed::<sr25519::Public>("Alice"),
+				DEFAULT_PARA_ID,
+			)
+		},
+		vec![],
+		None,
+		None,
+		None,
+		Some(properties),
+		Extensions {
+			relay_chain: "rococo_local_testnet".into(),
+			para_id: DEFAULT_PARA_ID.into(),
+		},
+	))
+}
+
+const CLN_COL_ACC_1: [u8; 32] = hex!["e6cf13c86a5f174acba79ca361dc429d89eb704c6a407af83f30b11ab8bc5045"];
+const CLN_COL_SESSION_1: [u8; 32] = hex!["e29df39b74777495ca00cd7a316ce98c5225d7088ae924b122fe0e2e6a4b5569"];
+const CLN_COL_ACC_2: [u8; 32] = hex!["e8ed0c2a40fb5a0bbb24c38f5c8cd83d79498ac029ac9f87497677f5701e3d2c"];
+const CLN_COL_SESSION_2: [u8; 32] = hex!["7cacfbce640321ba84a85f41dfb43c2a2ea14ed789c096ad62ee0491599b0f44"];
+
+pub fn get_chain_spec_cln() -> Result<ChainSpec, String> {
+	let properties = get_properties("WILT", 15, 38);
+	let wasm = WASM_BINARY.ok_or("No WASM")?;
+	let id: ParaId = 2085.into();
+
+	Ok(ChainSpec::from_genesis(
+		"Clone Chain",
+		"cln_kilt",
+		ChainType::Live,
+		move || {
+			testnet_genesis(
+				wasm,
+				vec![
+					(CLN_COL_ACC_1.into(), None, 30000 * KILT),
+					(CLN_COL_ACC_2.into(), None, 30000 * KILT),
+				],
+				kilt_inflation_config(),
+				MAX_COLLATOR_STAKE,
+				vec![
+					(CLN_COL_ACC_1.into(), CLN_COL_SESSION_1.unchecked_into()),
+					(CLN_COL_ACC_2.into(), CLN_COL_SESSION_2.unchecked_into()),
+				],
+				vec![
+					(CLN_COL_ACC_1.into(), 40000 * KILT),
+					(CLN_COL_ACC_2.into(), 40000 * KILT),
+				],
+				CLN_COL_ACC_1.into(),
+				id,
+			)
+		},
+		vec![
+			"/dns4/bootnode.kilt.io/tcp/30360/p2p/12D3KooWRPR7q1Rgwurd4QGyUUbVnN4nXYNVzbLeuhFsd9eXmHJk"
+				.parse()
+				.expect("bootnode address is formatted correctly; qed"),
+			"/dns4/bootnode.kilt.io/tcp/30361/p2p/12D3KooWDAEqpTRsL76itsabbh4SeaqtCM6v9npQ8eCeqPbbuFE9"
+				.parse()
+				.expect("bootnode address is formatted correctly; qed"),
+		],
+		Some(TelemetryEndpoints::new(vec![(TELEMETRY_URL.to_string(), 0)]).expect("WILT telemetry url is valid; qed")),
+		None,
+		None,
+		Some(properties),
+		Extensions {
+			relay_chain: "westend".into(),
+			para_id: id.into(),
+		},
+	))
+}
+
+// pub fn load_clone_spec() -> Result<ChainSpec, String> {
+// 	ChainSpec::from_json_bytes(&include_bytes!("../../res/clone.json")[..])
+// }
+
+#[allow(clippy::too_many_arguments)]
+fn testnet_genesis(
+	wasm_binary: &[u8],
+	stakers: Vec<(AccountId, Option<AccountId>, Balance)>,
+	inflation_config: InflationInfo,
+	max_candidate_stake: Balance,
+	initial_authorities: Vec<(AccountId, AuthorityId)>,
+	endowed_accounts: Vec<(AccountId, Balance)>,
+	sudo: AccountId,
+	id: ParaId,
+) -> GenesisConfig {
+	GenesisConfig {
+		system: SystemConfig {
+			code: wasm_binary.to_vec(),
+		},
+		balances: BalancesConfig {
+			balances: endowed_accounts.iter().cloned().collect(),
+		},
+		parachain_info: ParachainInfoConfig { parachain_id: id },
+		vesting: Default::default(),
+		parachain_staking: ParachainStakingConfig {
+			stakers: stakers.clone(),
+			inflation_config,
+			max_candidate_stake,
+		},
+		aura: Default::default(),
+		aura_ext: Default::default(),
+		parachain_system: Default::default(),
+		collator_selection: CollatorSelectionConfig {
+			invulnerables: stakers.iter().map(|(acc, _, _)| acc).cloned().collect(),
+			candidacy_bond: MinCollatorStake::get(),
+			desired_candidates: 2,
+		},
+		session: SessionConfig {
+			keys: initial_authorities
+				.iter()
+				.map(|(acc, key)| {
+					(
+						acc.clone(),
+						acc.clone(),
+						clone_runtime::SessionKeys { aura: key.clone() },
+					)
+				})
+				.collect::<Vec<_>>(),
+		},
+		sudo: SudoConfig { key: Some(sudo) },
+		council: CouncilConfig {
+			members: initial_authorities.iter().map(|(acc, _)| acc).cloned().collect(),
+			phantom: Default::default(),
+		},
+		technical_committee: TechnicalCommitteeConfig {
+			members: initial_authorities.iter().map(|(acc, _)| acc).cloned().collect(),
+			phantom: Default::default(),
+		},
+		treasury: Default::default(),
+		technical_membership: Default::default(),
+		tips_membership: Default::default(),
+		democracy: Default::default(),
+		polkadot_xcm: PolkadotXcmConfig {
+			safe_xcm_version: Some(SAFE_XCM_VERSION),
+		},
+	}
+}
