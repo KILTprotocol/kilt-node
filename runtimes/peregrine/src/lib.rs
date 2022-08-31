3bfb3585ab6fde043b723a0d7607a0632d92e6a6
William Freudenberger <william@kilt.io>
2022-08-31 14:35:48 +0200

    feat: add migration extrinsics (#398)
    * feat: clear up runtime selection
    
    * feat: s2p
    
    * fix: runtime selection
    
    * feat: add relay call builder
    
    * wip: add migraton init extrinsics
    
    * feat: add relay block check setters, xcm
    
    * docs: update pallet description
    
    * docs: add comment to CheckAssociatedRelayNumber
    
    * fix: native origin
    
    * bench: add extrinsic weight estimates
    
    * fix: deps
    
    Co-authored-by: weichweich <albrecht@kilt.io>

--- runtimes/peregrine/src/lib.rs
+++ runtimes/peregrine/src/lib.rs
@@ -46,6 +46,8 @@ use xcm_executor::XcmExecutor;
 
 use delegation::DelegationAc;
 pub use parachain_staking::InflationInfo;
+
+use kilt_support::relay::RelayChainCallBuilder;
 use runtime_common::{
 	authorization::{AuthorizationId, PalletAuthorize},
 	constants::{self, EXISTENTIAL_DEPOSIT, KILT},
@@ -216,7 +218,9 @@ impl cumulus_pallet_parachain_system::Config for Runtime {
 	type ReservedDmpWeight = ReservedDmpWeight;
 	type XcmpMessageHandler = XcmpQueue;
 	type ReservedXcmpWeight = ReservedXcmpWeight;
-	type CheckAssociatedRelayNumber = cumulus_pallet_parachain_system::RelayNumberStrictlyIncreases;
+	// We temporarily control this via the RelayMigration pallet which can toggle
+	// between strict and any.
+	type CheckAssociatedRelayNumber = RelayMigration;
 }
 
 impl parachain_info::Config for Runtime {}
@@ -661,6 +665,12 @@ impl parachain_staking::Config for Runtime {
 	const BLOCKS_PER_YEAR: Self::BlockNumber = constants::BLOCKS_PER_YEAR;
 }
 
+impl pallet_relay_migration::Config for Runtime {
+	type Event = Event;
+	type ApproveOrigin = ApproveOrigin;
+	type RelayChainCallBuilder = RelayChainCallBuilder<Runtime, ParachainInfo>;
+}
+
 impl pallet_utility::Config for Runtime {
 	type Event = Event;
 	type Call = Call;
@@ -880,6 +890,7 @@ construct_runtime! {
 		// placeholder: parachain council election = 33,
 		TechnicalMembership: pallet_membership::<Instance1> = 34,
 		Treasury: pallet_treasury = 35,
+		RelayMigration: pallet_relay_migration::{Pallet, Call, Storage, Event<T>} = 36,
 
 		// Utility module.
 		Utility: pallet_utility = 40,
--- runtimes/spiritnet/src/lib.rs
+++ runtimes/spiritnet/src/lib.rs
@@ -46,6 +46,8 @@ use xcm_executor::XcmExecutor;
 
 use delegation::DelegationAc;
 pub use parachain_staking::InflationInfo;
+
+use kilt_support::relay::RelayChainCallBuilder;
 use runtime_common::{
 	authorization::{AuthorizationId, PalletAuthorize},
 	constants::{self, EXISTENTIAL_DEPOSIT, KILT},
@@ -211,7 +213,9 @@ impl cumulus_pallet_parachain_system::Config for Runtime {
 	type ReservedDmpWeight = ReservedDmpWeight;
 	type XcmpMessageHandler = XcmpQueue;
 	type ReservedXcmpWeight = ReservedXcmpWeight;
-	type CheckAssociatedRelayNumber = cumulus_pallet_parachain_system::RelayNumberStrictlyIncreases;
+	// We temporarily control this via the RelayMigration pallet which can toggle
+	// between strict and any.
+	type CheckAssociatedRelayNumber = RelayMigration;
 }
 
 impl parachain_info::Config for Runtime {}
@@ -656,6 +660,12 @@ impl parachain_staking::Config for Runtime {
 	const BLOCKS_PER_YEAR: Self::BlockNumber = constants::BLOCKS_PER_YEAR;
 }
 
+impl pallet_relay_migration::Config for Runtime {
+	type Event = Event;
+	type ApproveOrigin = ApproveOrigin;
+	type RelayChainCallBuilder = RelayChainCallBuilder<Runtime, ParachainInfo>;
+}
+
 impl pallet_utility::Config for Runtime {
 	type Event = Event;
 	type Call = Call;
@@ -873,6 +883,7 @@ construct_runtime! {
 		// placeholder: parachain council election = 33,
 		TechnicalMembership: pallet_membership::<Instance1> = 34,
 		Treasury: pallet_treasury = 35,
+		RelayMigration: pallet_relay_migration::{Pallet, Call, Storage, Event<T>} = 36,
 
 		// Utility module.
 		Utility: pallet_utility = 40,
@@ -906,16 +917,16 @@ construct_runtime! {
 
 		// Parachains pallets. Start indices at 80 to leave room.
 
-		// Many other things but also: Send and receive DMP and XCMP messages
+		// Among others: Send and receive DMP and XCMP messages.
 		ParachainSystem: cumulus_pallet_parachain_system = 80,
 		ParachainInfo: parachain_info::{Pallet, Storage, Config} = 81,
 		// Wrap and unwrap XCMP messages to send and receive them. Queue them for later processing.
 		XcmpQueue: cumulus_pallet_xcmp_queue::{Pallet, Call, Storage, Event<T>} = 82,
-		// Build XCM scripts
+		// Build XCM scripts.
 		PolkadotXcm: pallet_xcm::{Pallet, Call, Event<T>, Origin, Config} = 83,
-		// Does nothing cool. Provides an origin...
+		// Does nothing cool, just provides an origin.
 		CumulusXcm: cumulus_pallet_xcm::{Pallet, Event<T>, Origin} = 84,
-		// Queue and DMP messages and pass them on to be executed
+		// Queue and pass DMP messages on to be executed.
 		DmpQueue: cumulus_pallet_dmp_queue::{Pallet, Call, Storage, Event<T>} = 85,
 	}
 }
