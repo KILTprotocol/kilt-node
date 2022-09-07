a18bca5205ec3ae9fa50a8d9cbff5c6e6e0bb352
William Freudenberger <william@kilt.io>
2022-09-07 13:54:55 +0200

    style: ignore clippy in default weights

--- pallets/parachain-staking/src/default_weights.rs
+++ pallets/parachain-staking/src/default_weights.rs
@@ -40,6 +40,7 @@
 #![allow(unused_parens)]
 #![allow(unused_imports)]
 #![allow(clippy::unnecessary_cast)]
+#![allow(clippy::unnecessary_cast)]
 
 use frame_support::{traits::Get, weights::{Weight, constants::RocksDbWeight}};
 use sp_std::marker::PhantomData;
