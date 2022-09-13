bc078da0215e3d46e6b96775cbbb9a4368d68f2f
gera <gerared45@gmail.com>
2022-09-13 10:46:40 +0200

    chore: single-line

--- .maintain/docker-auth-config.sh
+++ .maintain/docker-auth-config.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-export ENCODED_PASSWORD= `echo -n "AWS:$(aws ecr get-login-password --region $AWS_DEFAULT_REGION)" | base64`
-export PAYLOAD=`jq -n --arg userpass $ENCODED_PASSWORD '{"auths": {"'$AWS_REGISTRY'": {"auth": $userpass}}}'`
+export PAYLOAD=`jq -n --arg userpass "$(echo -n "AWS:$(aws ecr get-login-password --region $AWS_DEFAULT_REGION)" | base64)" '{"auths": {"'$AWS_REGISTRY'": {"auth": $userpass}}}'`
 
 curl --request PUT --header "PRIVATE-TOKEN:$TOKEN" "https://gitlab.com/api/v4/projects/26909212/variables/DOCKER_AUTH_CONFIG" --form "value=$PAYLOAD"
