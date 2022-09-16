e92d9c33254d6f97865d94e7d197d15c9b02e921
William Freudenberger <william@kilt.io>
2022-09-16 16:28:05 +0200

    feat: write delegator state migration

--- pallets/parachain-staking/src/lib.rs
+++ pallets/parachain-staking/src/lib.rs
@@ -119,6 +119,7 @@
 pub mod benchmarking;
 pub mod default_weights;
 
+pub mod migration;
 #[cfg(test)]
 pub(crate) mod mock;
 #[cfg(test)]
@@ -171,7 +172,7 @@ pub mod pallet {
 	pub(crate) const STAKING_ID: LockIdentifier = *b"kiltpstk";
 
 	/// The current storage version.
-	const STORAGE_VERSION: StorageVersion = StorageVersion::new(7);
+	const STORAGE_VERSION: StorageVersion = StorageVersion::new(8);
 
 	/// Pallet for parachain staking.
 	#[pallet::pallet]
