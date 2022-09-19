02eb40733520f5b4a5d18378e2bcbcfd3c79ff43
William Freudenberger <william@kilt.io>
2022-09-19 10:22:13 +0200

    stlye: fmt

--- pallets/parachain-staking/src/tests.rs
+++ pallets/parachain-staking/src/tests.rs
@@ -4298,14 +4298,8 @@ fn api_get_unclaimed_staking_rewards() {
 			let rewards_1 = StakePallet::get_unclaimed_staking_rewards(&1);
 			let rewards_2 = StakePallet::get_unclaimed_staking_rewards(&2);
 			let rewards_3 = StakePallet::get_unclaimed_staking_rewards(&3);
-			assert_eq!(
-				2 * rewards_1,
-				rewards_3,
-			);
-			assert_eq!(
-				rewards_2,
-				inflation_config.delegator.reward_rate.per_block * stake * 2
-			);
+			assert_eq!(2 * rewards_1, rewards_3,);
+			assert_eq!(rewards_2, inflation_config.delegator.reward_rate.per_block * stake * 2);
 
 			// API and actual claiming should match
 			assert_ok!(StakePallet::increment_collator_rewards(Origin::signed(1)));
