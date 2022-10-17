dba1f0021b4aad139434c1d0b6dc8d81535d0e4e
William Freudenberger <william@kilt.io>
2022-10-17 15:31:50 +0200

    chore: version 1.7.5

--- runtimes/peregrine/src/lib.rs
+++ runtimes/peregrine/src/lib.rs
@@ -84,7 +84,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("mashnet-node"),
 	impl_name: create_runtime_str!("mashnet-node"),
 	authoring_version: 4,
-	spec_version: 10740,
+	spec_version: 10750,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 4,
