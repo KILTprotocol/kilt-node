3ea52f8e092b05df962bad0dbfdfb5628715ec90
William Freudenberger <william@kilt.io>
2022-10-24 09:24:54 +0200

    refactor: remove custom (de)serialization

--- support/src/deposit.rs
+++ support/src/deposit.rs
@@ -23,14 +23,8 @@ use sp_runtime::{traits::Zero, DispatchError};
 
 /// An amount of balance reserved by the specified address.
 #[derive(Clone, Debug, Encode, Decode, Eq, PartialEq, Ord, PartialOrd, TypeInfo, MaxEncodedLen)]
-#[cfg_attr(feature = "std", derive(serde::Serialize, serde::Deserialize))]
 pub struct Deposit<Account, Balance> {
 	pub owner: Account,
-	#[cfg_attr(
-		feature = "std",
-		serde(bound(serialize = "Balance: std::fmt::Display", deserialize = "Balance: std::str::FromStr"))
-	)]
-	#[cfg_attr(feature = "std", serde(with = "serde_balance"))]
 	pub amount: Balance,
 }
 
@@ -48,58 +42,4 @@ pub fn reserve_deposit<Account, Currency: ReservableCurrency<Account>>(
 pub fn free_deposit<Account, Currency: ReservableCurrency<Account>>(deposit: &Deposit<Account, Currency::Balance>) {
 	let err_amount = Currency::unreserve(&deposit.owner, deposit.amount);
 	debug_assert!(err_amount.is_zero());
-}
-
-// This code was copied from https://github.com/paritytech/substrate/blob/ded44948e2d5a398abcb4e342b0513cb690961bb/frame/transaction-payment/src/types.rs#L113
-// It is needed because u128 cannot be serialized by serde_json out of the box.
-#[cfg(feature = "std")]
-mod serde_balance {
-	use serde::{Deserialize, Deserializer, Serializer};
-
-	pub fn serialize<S: Serializer, T: std::fmt::Display>(t: &T, serializer: S) -> Result<S::Ok, S::Error> {
-		serializer.serialize_str(&t.to_string())
-	}
-
-	pub fn deserialize<'de, D: Deserializer<'de>, T: std::str::FromStr>(deserializer: D) -> Result<T, D::Error> {
-		let s = String::deserialize(deserializer)?;
-		s.parse::<T>()
-			.map_err(|_| serde::de::Error::custom("Parse from string failed"))
-	}
-}
-
-#[cfg(test)]
-mod tests {
-	use super::*;
-
-	#[test]
-	fn should_serialize_and_deserialize_properly_with_string() {
-		let deposit = Deposit {
-			owner: 0_u8,
-			amount: 0_u128,
-		};
-
-		let json_str = r#"{"owner":0,"amount":"0"}"#;
-
-		assert_eq!(serde_json::to_string(&deposit).unwrap(), json_str);
-		assert_eq!(serde_json::from_str::<Deposit<u8, u128>>(json_str).unwrap(), deposit);
-
-		// should not panic
-		serde_json::to_value(&deposit).unwrap();
-	}
-
-	#[test]
-	fn should_serialize_and_deserialize_properly_large_value() {
-		let deposit = Deposit {
-			owner: 0_u8,
-			amount: u128::MAX,
-		};
-
-		let json_str = r#"{"owner":0,"amount":"340282366920938463463374607431768211455"}"#;
-
-		assert_eq!(serde_json::to_string(&deposit).unwrap(), json_str);
-		assert_eq!(serde_json::from_str::<Deposit<u8, u128>>(json_str).unwrap(), deposit);
-
-		// should not panic
-		serde_json::to_value(&deposit).unwrap();
-	}
 }
