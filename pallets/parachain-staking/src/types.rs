ac29b18aed185a95a9ab1c100fb943609b05e3bb
William Freudenberger <william@kilt.io>
2022-11-04 10:24:59 +0100

    feat: workspace inheritance (#430)
    ## fixes KILTProtocol/ticket#2217
    * Applies the [`workspace
    inheritance`](https://doc.rust-lang.org/cargo/reference/workspaces.html)
    feature of Rust 1.64.0: All dependencies sources are declared exactly
    once in our root `Cargo.toml` and downstreamed via `workspace = true`
    * Version, authors, website, repo, license, edition, readme are also
    declared once and downstreamed
    * Since **features in the root definition are additive**, I decided to
    not enforce any features (except for disabling `std` for all crates
    which are used outside of our client).
    * IMO, these features should rather be declared in the corresponding
    crates such that it is clear which ones are used where.
    * In the case of `serde`, `codec` and `scale-info` we could import
    features into root, because these are always used.
     * Normalizes structure of all of our crate tomls
     * Adds description to crates which didn't have that yet
    * Exposes more endpoints for all pallets, e.g. Documentation, Website,
    ...
    
    ## How to review efficiently
    
    You should focus on the [root
    Cargo.toml](https://github.com/KILTprotocol/kilt-node/pull/430/files#diff-2e9d962a08321605940b5a657135052fbcef87b5e360662bb527c96d9a615542)
    and my text above, e.g. what is our policy in regards to default crate
    features.
    
    ## Checklist:
    
    - [x] I have verified that the code works
    - [x] No panics! (checked arithmetic ops, no indexing `array[3]` use
    `get(3)`, ...)
    - [x] I have verified that the code is easy to understand
    - [x] I have [left the code in a better
    state](https://deviq.com/principles/boy-scout-rule)
    - [x] I have documented the changes (where applicable)

--- pallets/parachain-staking/src/types.rs
+++ pallets/parachain-staking/src/types.rs
@@ -16,8 +16,8 @@
 
 // If you feel like getting in touch with us, you can do so at info@botlabs.org
 
+use codec::{Decode, Encode, MaxEncodedLen};
 use frame_support::traits::{Currency, Get};
-use parity_scale_codec::{Decode, Encode, MaxEncodedLen};
 use scale_info::TypeInfo;
 use sp_runtime::{
 	traits::{AtLeast32BitUnsigned, CheckedSub, Saturating, Zero},
