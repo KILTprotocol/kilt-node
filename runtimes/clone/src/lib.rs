0d0044b9f121076706ca19417364ce67954f8ccc
William Freudenberger <william@kilt.io>
2022-11-14 16:52:02 +0100

    chore: bump spec versions to 10902

--- runtimes/clone/src/lib.rs
+++ runtimes/clone/src/lib.rs
@@ -79,7 +79,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("KILT"),
 	impl_name: create_runtime_str!("KILT"),
 	authoring_version: 0,
-	spec_version: 10900,
+	spec_version: 10902,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 0,
