d8612346d4755a73501224d9b678613e425d8537
Albrecht <albrecht@kilt.io>
2023-01-05 15:20:30 +0100

    chore: update 2023 (#445)
    ## related https://github.com/KILTprotocol/ticket/issues/2356
    
    
    ## Checklist:
    
    - [ ] I have verified that the code works
    - [ ] No panics! (checked arithmetic ops, no indexing `array[3]` use
    `get(3)`, ...)
    - [ ] I have verified that the code is easy to understand
      - [ ] If not, I have left a well-balanced amount of inline comments
    - [ ] I have [left the code in a better
    state](https://deviq.com/principles/boy-scout-rule)
    - [ ] I have documented the changes (where applicable)

--- runtimes/spiritnet/src/weights/pallet_membership.rs
+++ runtimes/spiritnet/src/weights/pallet_membership.rs
@@ -1,5 +1,5 @@
 // KILT Blockchain – https://botlabs.org
-// Copyright (C) 2019-2022 BOTLabs GmbH
+// Copyright (C) 2019-2023 BOTLabs GmbH
 
 // The KILT Blockchain is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
