677159c8d160a44bf86f65b427a6eef52022e3b8
gera <gerared45@gmail.com>
2023-01-27 11:47:06 +0100

    chore: publish metadata as gitlab artifact

--- .maintain/s3-upload.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-aws configure set region eu-central-1
-
-if [[ "$1" == "spiritnet" ]]; then
-aws s3 cp /tmp/spiritnet-metadata.json s3://$S3_BUCKET/spiritnet/$2/
-else
-aws s3 cp /tmp/peregrine-metadata.json s3://$S3_BUCKET/peregrine/$2/
-fi
