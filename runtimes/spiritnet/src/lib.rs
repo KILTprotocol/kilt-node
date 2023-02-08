700d9058ccf5bb510c9dd72e2e77b85292ce55d4
Antonio Antonino <antonio@kilt.io>
2023-02-08 13:57:05 +0100

    chore: bump spec and crate versions to next release cycle 1.10.0-dev

--- runtimes/spiritnet/src/lib.rs
+++ runtimes/spiritnet/src/lib.rs
@@ -93,7 +93,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("kilt-spiritnet"),
 	impl_name: create_runtime_str!("kilt-spiritnet"),
 	authoring_version: 1,
-	spec_version: 10900,
+	spec_version: 11000,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 4,
