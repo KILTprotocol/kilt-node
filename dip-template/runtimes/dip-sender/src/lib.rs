b5fd68f44234998acae4d4839b99ee0d978d2241
Antonio Antonino <antonio@kilt.io>
2023-03-10 17:46:56 +0100

    More fixess

--- dip-template/runtimes/dip-sender/src/lib.rs
+++ dip-template/runtimes/dip-sender/src/lib.rs
@@ -180,8 +180,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("template-parachain"),
 	impl_name: create_runtime_str!("template-parachain"),
 	authoring_version: 1,
-	// TODO: Bump back to 1
-	spec_version: 2,
+	spec_version: 1,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	// TODO: Bump back to 1
