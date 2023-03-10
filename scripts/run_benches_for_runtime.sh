03b798c19f16995647b118da750c0d41fee66010
weichweich <albrecht@kilt.io>
2023-03-10 15:40:35 +0100

    feat: PoV size benchmarks

--- scripts/run_benches_for_runtime.sh
+++ scripts/run_benches_for_runtime.sh
@@ -9,30 +9,30 @@ chain=$([ "$1" == "spiritnet" ] && echo "spiritnet-dev" || echo "dev")
 standard_args="--release --locked --features=runtime-benchmarks --bin=kilt-parachain"
 
 pallets=(
-    attestation
-    ctype
-    delegation
-    did
-    frame-system
-    pallet-balances
-    pallet-collective
-    pallet-democracy
+	attestation
+	ctype
+	delegation
+	did
+	frame-system
+	pallet-balances
+	pallet-collective
+	pallet-democracy
 	pallet-did-lookup
-    pallet-indices
+	pallet-indices
 	pallet-inflation
-    pallet-membership
-    pallet-preimage
+	pallet-membership
+	pallet-preimage
 	pallet-proxy
-    pallet-scheduler
-    pallet-session
-    pallet-timestamp
+	pallet-scheduler
+	pallet-session
+	pallet-timestamp
 	pallet-tips
-    pallet-treasury
-    pallet-utility
-    pallet-vesting
+	pallet-treasury
+	pallet-utility
+	pallet-vesting
 	pallet-web3-names
 	pallet-xcm
-    parachain-staking
+	parachain-staking
 	public-credentials
 )
 
@@ -41,17 +41,18 @@ echo "[+] Running all runtime benchmarks for $runtime --chain=$chain"
 cargo build $standard_args
 
 for pallet in "${pallets[@]}"; do
-    echo "Runtime: $runtime. Pallet: $pallet";
-    # shellcheck disable=SC2086
-    ./target/release/kilt-parachain benchmark pallet \
-    --chain="${chain}" \
-    --steps=50 \
-    --repeat=20 \
-    --pallet="$pallet" \
-    --extrinsic="*" \
-    --execution=wasm \
-    --wasm-execution=compiled \
-    --heap-pages=4096 \
-    --output="./runtimes/${runtime}/src/weights/${pallet//-/_}.rs" \
-    --template=".maintain/runtime-weight-template.hbs"
+	echo "Runtime: $runtime. Pallet: $pallet"
+	# shellcheck disable=SC2086
+	./target/release/kilt-parachain benchmark pallet \
+		--template=".maintain/runtime-weight-template.hbs" \
+		--header="HEADER-GPL" \
+		--execution=wasm \
+		--wasm-execution=compiled \
+		--heap-pages=4096 \
+		--steps=50 \
+		--repeat=20 \
+		--chain="${chain}" \
+		--pallet="$pallet" \
+		--extrinsic="*" \
+		--output="./runtimes/${runtime}/src/weights/${pallet//-/_}.rs"
 done
