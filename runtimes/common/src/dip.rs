40a0c28ef0dae26d705f52fe9658f84a4ac2fafd
Antonio Antonino <antonio@kilt.io>
2023-03-21 11:30:46 +0100

    wip pt. 2

--- runtimes/common/src/dip.rs
+++ runtimes/common/src/dip.rs
@@ -21,15 +21,63 @@ use sp_std::marker::PhantomData;
 pub mod sender {
 	use super::*;
 
-	use codec::Encode;
-	use did::did_details::{DidDetails, DidPublicKeyDetails};
+	use codec::{Decode, Encode};
+	use did::{
+		did_details::{DidDetails, DidPublicKeyDetails},
+		DidVerificationKeyRelationship, KeyIdOf,
+	};
 	use pallet_dip_sender::traits::{IdentityProofGenerator, IdentityProvider};
 
-	pub(crate) const AUTH_KEY_LEAF: &[u8] = b"auth";
-	pub(crate) const ATT_KEY_LEAF: &[u8] = b"att";
-	pub(crate) const DEL_KEY_LEAF: &[u8] = b"del";
-	pub(crate) const ENC_KEY_PREFIX: &[u8] = b"enc-";
-	pub(crate) const PUB_KEY_PREFIX: &[u8] = b"pub-";
+	pub enum MerkleLeaf<T: did::Config> {
+		VerificationKey(KeyIdOf<T>, DidVerificationKeyRelationship),
+		EncryptionKey(KeyIdOf<T>),
+		PublicKey(KeyIdOf<T>, DidPublicKeyDetails<T::BlockNumber>),
+	}
+
+	impl<T> MerkleLeaf<T>
+	where
+		T: did::Config,
+	{
+		pub fn key(&self) -> Vec<u8> {
+			match self {
+				MerkleLeaf::VerificationKey(_, relationship) => relationship.encode(),
+				MerkleLeaf::EncryptionKey(key_id) => key_id.encode(),
+				MerkleLeaf::PublicKey(key_id, _) => key_id.encode(),
+			}
+		}
+
+		pub fn value(&self) -> Vec<u8> {
+			match self {
+				MerkleLeaf::VerificationKey(key_id, relationship) => key_id.encode(),
+				MerkleLeaf::EncryptionKey(key_id) => key_id.encode(),
+				MerkleLeaf::PublicKey(_, public_key) => public_key.encode(),
+			}
+		}
+	}
+
+	impl<T> TryFrom<(Vec<u8>, Vec<u8>)> for MerkleLeaf<T>
+	where
+		T: did::Config,
+	{
+		// TODO: Proper error handling
+		type Error = ();
+
+		fn try_from((key, value): (Vec<u8>, Vec<u8>)) -> Result<Self, Self::Error> {
+			let (key, value) = (&mut key.as_slice(), &mut value.as_slice());
+			if let (Ok(relationship), Ok(key_id)) =
+				(DidVerificationKeyRelationship::decode(key), KeyIdOf::<T>::decode(value))
+			{
+				Ok(Self::VerificationKey(key_id, relationship))
+			} else if let (Ok(key_id), Ok(_)) = (KeyIdOf::<T>::decode(key), KeyIdOf::<T>::decode(value)) {
+				Ok(Self::EncryptionKey(key_id))
+			} else if let (Ok(key_id), Ok(public_key)) = (KeyIdOf::<T>::decode(key), DidPublicKeyDetails::decode(value))
+			{
+				Ok(Self::PublicKey(key_id, public_key))
+			} else {
+				Err(())
+			}
+		}
+	}
 
 	pub struct DidMerkleRootGenerator<T>(PhantomData<T>);
 
@@ -48,43 +96,66 @@ pub mod sender {
 			let mut trie_builder = TrieDBMutBuilder::<LayoutV1<T::Hashing>>::new(&mut db, &mut trie).build();
 
 			// Authentication key
+			let auth_key_leaf = MerkleLeaf::<T>::VerificationKey(
+				identity.authentication_key,
+				DidVerificationKeyRelationship::Authentication,
+			);
 			trie_builder
-				.insert(AUTH_KEY_LEAF, identity.authentication_key.encode().as_slice())
+				.insert(
+					auth_key_leaf.key().encode().as_slice(),
+					auth_key_leaf.value().encode().as_slice(),
+				)
 				.map_err(|_| ())?;
-			// Attestation key
-			if let Some(ref att_key) = identity.attestation_key {
+			// Attestation key: (key relationship, key id)
+			if let Some(att_key_id) = identity.attestation_key {
+				let att_key_leaf =
+					MerkleLeaf::<T>::VerificationKey(att_key_id, DidVerificationKeyRelationship::AssertionMethod);
 				trie_builder
-					.insert(ATT_KEY_LEAF, att_key.encode().as_slice())
+					.insert(
+						att_key_leaf.key().encode().as_slice(),
+						att_key_leaf.value().encode().as_slice(),
+					)
 					.map_err(|_| ())?;
 			};
-			// Delegation key
-			if let Some(ref del_key) = identity.delegation_key {
+			// Delegation key: (key relationship, key id)
+			if let Some(del_key_id) = identity.delegation_key {
+				let del_key_leaf =
+					MerkleLeaf::<T>::VerificationKey(del_key_id, DidVerificationKeyRelationship::CapabilityDelegation);
 				trie_builder
-					.insert(DEL_KEY_LEAF, del_key.encode().as_slice())
+					.insert(
+						del_key_leaf.key().encode().as_slice(),
+						del_key_leaf.value().encode().as_slice(),
+					)
 					.map_err(|_| ())?;
 			};
-			// Key agreement keys
+			// Key agreement keys [(enc-<key_id>, key id)]
 			identity
 				.key_agreement_keys
-				.iter()
-				.enumerate()
-				.try_for_each(|(i, id)| -> Result<(), ()> {
-					// Key leaf = "enc-<index>"
-					let key_leaf = [ENC_KEY_PREFIX, i.to_be_bytes().as_slice()].concat();
-					trie_builder.insert(&key_leaf, id.encode().as_slice()).map_err(|_| ())?;
-					Ok(())
-				})?;
-			// Public keys
-			identity.public_keys.iter().enumerate().try_for_each(
-				|(i, (id, DidPublicKeyDetails { key: public_key, .. }))| -> Result<(), ()> {
-					// Key leaf = "pub-<index>"
-					let key_leaf = [PUB_KEY_PREFIX, i.to_be_bytes().as_slice()].concat();
+				.into_iter()
+				.try_for_each(|id| -> Result<(), ()> {
+					let enc_key_leaf = MerkleLeaf::<T>::EncryptionKey(id);
 					trie_builder
-						.insert(&key_leaf, (id, public_key).encode().as_slice())
+						.insert(
+							enc_key_leaf.key().encode().as_slice(),
+							enc_key_leaf.value().encode().as_slice(),
+						)
 						.map_err(|_| ())?;
 					Ok(())
-				},
-			)?;
+				})?;
+			// Public keys: [(key id, public key)]
+			identity
+				.public_keys
+				.into_iter()
+				.try_for_each(|(id, key_details)| -> Result<(), ()> {
+					let pub_key_leaf = MerkleLeaf::<T>::PublicKey(id, key_details);
+					trie_builder
+						.insert(
+							pub_key_leaf.key().encode().as_slice(),
+							pub_key_leaf.value().encode().as_slice(),
+						)
+						.map_err(|_| ())?;
+					Ok(())
+				})?;
 			trie_builder.commit();
 			Ok(trie_builder.root().to_owned())
 		}
@@ -113,11 +184,14 @@ pub mod sender {
 }
 
 pub mod receiver {
+	use crate::dip::sender::MerkleLeaf;
+
 	use super::*;
 
 	use did::{did_details::DidPublicKey, DidVerificationKeyRelationship};
 	use dip_support::VersionedIdentityProof;
 	use pallet_dip_receiver::traits::IdentityProofVerifier;
+	use sp_std::vec::Vec;
 	use sp_trie::LayoutV1;
 
 	pub struct ProofEntry {
@@ -137,7 +211,7 @@ pub mod receiver {
 		type BlindedValue = Vec<Vec<u8>>;
 		type Error = ();
 		type LeafKey = Vec<u8>;
-		type LeafValue = Option<Vec<u8>>;
+		type LeafValue = Vec<u8>;
 		type ProofDigest = T::Hash;
 		type VerificationResult = VerificationResult;
 
@@ -145,21 +219,31 @@ pub mod receiver {
 			proof: VersionedIdentityProof<Self::BlindedValue, Self::LeafKey, Self::LeafValue>,
 			digest: Self::ProofDigest,
 		) -> Result<Self::VerificationResult, Self::Error> {
-			use crate::dip::sender::{ATT_KEY_LEAF, AUTH_KEY_LEAF, DEL_KEY_LEAF, ENC_KEY_PREFIX, PUB_KEY_PREFIX};
 			use dip_support::v1;
 			use sp_trie::verify_trie_proof;
 
 			let proof: v1::Proof<_, _, _> = proof.try_into()?;
-			verify_trie_proof::<LayoutV1<T::Hashing>, _, _, _>(&digest, &proof.blinded, &proof.revealed)
-				.map_err(|_| ())?;
-			let mut revealed_leafs: Vec<ProofEntry> = vec![];
+			verify_trie_proof::<LayoutV1<T::Hashing>, _, _, _>(
+				&digest,
+				&proof.blinded,
+				proof.revealed.iter().map(|(key, value)| (key, Some(value))).collect(),
+			)
+			.map_err(|_| ())?;
 
-			let revealed_leafs: Vec<ProofEntry> = proof.revealed.iter().map(|(key, value)| match (key) {
-				_ => ProofEntry {
-					key: value.try_into(),
-					verification_relationship: DidVerificationKeyRelationship::Authentication,
-				},
-			});
+			let revealed_leaves: Vec<MerkleLeaf<T>> = proof
+				.revealed
+				.iter()
+				.map(|(key, value)| MerkleLeaf::try_from((*key, *value)).expect("Error."))
+				.collect();
+			// .map(|(key, value)| match key.as_ref() {
+			// 	AUTH_KEY_LEAF => (
+			// 		KeyIdOf::<T>::from(key.as_slice()),
+			// 		DidVerificationKeyRelationship::Authentication,
+			// 	),
+			// 	ATT_KEY_LEAF => (*key,
+			// DidVerificationKeyRelationship::AssertionMethod), 	DEL_KEY_LEAF =>
+			// (*key, DidVerificationKeyRelationship::CapabilityDelegation), })
+			// .collect();
 		}
 	}
 }
