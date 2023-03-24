94ee61eb37b7355f9af0c3b840d0038e2e5e0caa
Antonio Antonino <antonio@kilt.io>
2023-03-24 16:43:20 +0100

    Set spec_version of templates to 11000 to avoid type definitions mixups

--- dip-template/runtimes/dip-receiver/src/lib.rs
+++ dip-template/runtimes/dip-receiver/src/lib.rs
@@ -150,7 +150,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("dip-receiver-runtime-template"),
 	impl_name: create_runtime_str!("dip-receiver-runtime-template"),
 	authoring_version: 1,
-	spec_version: 1,
+	spec_version: 11100,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
