079ffd2d5676bc7cfa74ce5d22630b8b8bbf13c1
Adel Golghalyani <ad96el@gmail.com>
2023-03-27 15:21:44 +0200

    intermediate prototype

--- sdk-wasm/src/lib.rs
+++ sdk-wasm/src/lib.rs
@@ -1,4 +1,5 @@
 mod utils;
+
 use kilt_utils::calculate_key_id;
 use sp_runtime::traits::BlakeTwo256;
 use wasm_bindgen::prelude::*;
@@ -6,6 +7,16 @@ use wasm_bindgen::prelude::*;
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+pub fn init() {
+	std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+}
+
+#[wasm_bindgen]
+extern "C" {
+	fn alert(s: &str);
+}
 
 #[wasm_bindgen]
 pub fn calculate_key(key: &str) -> Vec<u8> {
