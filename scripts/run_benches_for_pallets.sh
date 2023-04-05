d0d985747bfb030158b77393927e316d018063fa
weichweich <albrecht@kilt.io>
2023-04-05 12:54:06 +0200

    Dummy weights

--- scripts/run_benches_for_pallets.sh
+++ scripts/run_benches_for_pallets.sh
@@ -9,15 +9,15 @@ chain=$([ "$1" == "spiritnet" ] && echo "spiritnet-dev" || echo "dev")
 standard_args="--locked --features=runtime-benchmarks --bin=kilt-parachain"
 
 pallets=(
-	# attestation
+	attestation
 	ctype
-	# delegation
-	# did
-	# pallet-did-lookup
-	# pallet-inflation
-	# pallet-web3-names
-	# parachain-staking
-	# public-credentials
+	delegation
+	did
+	pallet-did-lookup
+	pallet-inflation
+	pallet-web3-names
+	parachain-staking
+	public-credentials
 )
 
 echo "[+] Running all default weight benchmarks for $runtime --chain=$chain"
