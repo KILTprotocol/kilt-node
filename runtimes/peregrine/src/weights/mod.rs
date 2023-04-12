cb071ff39ad4c99ce7f6a8965909ffbee3dbb6b1
Adel Golghalyani <48685760+Ad96el@users.noreply.github.com>
2023-04-12 10:56:42 +0200

    feat: add multisig pallet to runtime (#498)

--- runtimes/peregrine/src/weights/mod.rs
+++ runtimes/peregrine/src/weights/mod.rs
@@ -28,6 +28,7 @@ pub mod pallet_did_lookup;
 pub mod pallet_indices;
 pub mod pallet_inflation;
 pub mod pallet_membership;
+pub mod pallet_multisig;
 pub mod pallet_preimage;
 pub mod pallet_proxy;
 pub mod pallet_scheduler;
