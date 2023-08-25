6d29735f345932781d19c80a5866e4171e66550c
Antonio Antonino <antonio@kilt.io>
2023-08-25 11:17:13 +0200

    Add provider pallet to Peregrine

---
+++ runtimes/peregrine/src/dip.rs
@@ -0,0 +1,35 @@
+// KILT Blockchain – https://botlabs.org
+// Copyright (C) 2019-2023 BOTLabs GmbH
+
+// The KILT Blockchain is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// The KILT Blockchain is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+// If you feel like getting in touch with us, you can do so at info@botlabs.org
+
+use did::{DidRawOrigin, EnsureDidOrigin};
+use runtime_common::{
+	dip::{did::LinkedDidInfoProviderOf, merkle::DidMerkleRootGenerator},
+	AccountId, DidIdentifier, Hash,
+};
+
+use crate::{Runtime, RuntimeEvent};
+
+impl pallet_dip_provider::Config for Runtime {
+	type CommitOrigin = DidRawOrigin<DidIdentifier, AccountId>;
+	type CommitOriginCheck = EnsureDidOrigin<DidIdentifier, AccountId>;
+	type Identifier = DidIdentifier;
+	type IdentityCommitment = Hash;
+	type IdentityCommitmentGenerator = DidMerkleRootGenerator<Runtime>;
+	type IdentityProvider = LinkedDidInfoProviderOf<Runtime>;
+	type RuntimeEvent = RuntimeEvent;
+}
