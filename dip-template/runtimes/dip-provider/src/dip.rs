ba54dab85061437192d2a234fb6d3e5c22a01a03
Antonio Antonino <antonio@kilt.io>
2023-11-01 13:19:07 +0100

    WIP implementation for commitment storage deposit manager

--- dip-template/runtimes/dip-provider/src/dip.rs
+++ dip-template/runtimes/dip-provider/src/dip.rs
@@ -18,7 +18,10 @@
 
 use did::{DidRawOrigin, EnsureDidOrigin, KeyIdOf};
 use pallet_did_lookup::linkable_account::LinkableAccountId;
-use pallet_dip_provider::{traits::IdentityProvider, IdentityCommitmentVersion};
+use pallet_dip_provider::{
+	traits::{IdentityProvider, NoopHooks},
+	IdentityCommitmentVersion,
+};
 use parity_scale_codec::{Decode, Encode};
 use runtime_common::dip::{
 	did::LinkedDidInfoProviderOf,
@@ -29,20 +32,152 @@ use sp_std::vec::Vec;
 
 use crate::{AccountId, DidIdentifier, Hash, Runtime, RuntimeEvent};
 
-#[derive(Encode, Decode, TypeInfo)]
-pub struct RuntimeApiDipProofRequest {
-	pub(crate) identifier: DidIdentifier,
-	pub(crate) version: IdentityCommitmentVersion,
-	pub(crate) keys: Vec<KeyIdOf<Runtime>>,
-	pub(crate) accounts: Vec<LinkableAccountId>,
-	pub(crate) should_include_web3_name: bool,
+pub mod runtime_api {
+	use super::*;
+
+	#[derive(Encode, Decode, TypeInfo)]
+	pub struct DipProofRequest {
+		pub(crate) identifier: DidIdentifier,
+		pub(crate) version: IdentityCommitmentVersion,
+		pub(crate) keys: Vec<KeyIdOf<Runtime>>,
+		pub(crate) accounts: Vec<LinkableAccountId>,
+		pub(crate) should_include_web3_name: bool,
+	}
+
+	#[derive(Encode, Decode, TypeInfo)]
+	pub enum DipProofError {
+		IdentityNotFound,
+		IdentityProviderError(<LinkedDidInfoProviderOf<Runtime> as IdentityProvider<DidIdentifier>>::Error),
+		MerkleProofError(DidMerkleProofError),
+	}
 }
 
-#[derive(Encode, Decode, TypeInfo)]
-pub enum RuntimeApiDipProofError {
-	IdentityNotFound,
-	IdentityProviderError(<LinkedDidInfoProviderOf<Runtime> as IdentityProvider<DidIdentifier>>::Error),
-	MerkleProofError(DidMerkleProofError),
+pub mod deposit {
+	use super::*;
+	use crate::{Balance, Balances, RuntimeHoldReason};
+
+	use frame_support::traits::{fungible::Inspect, tokens::fungible::MutateHold};
+
+	use kilt_support::{traits::StorageDepositCollector, Deposit};
+	use pallet_dip_provider::{traits::ProviderHooks, HoldReason};
+	use sp_runtime::DispatchError;
+
+	pub enum CommitmentDepositCollectorError {
+		Internal,
+	}
+
+	impl From<CommitmentDepositCollectorError> for u16 {
+		fn from(value: CommitmentDepositCollectorError) -> Self {
+			match value {
+				CommitmentDepositCollectorError::Internal => u16::MAX,
+			}
+		}
+	}
+
+	impl From<CommitmentDepositCollectorError> for DispatchError {
+		fn from(value: CommitmentDepositCollectorError) -> Self {
+			match value {
+				CommitmentDepositCollectorError::Internal => {
+					DispatchError::Other("CommitmentDepositCollectorError::Internal")
+				}
+			}
+		}
+	}
+
+	pub const DEPOSIT: Balance = 100_000;
+
+	// TODO: Store deposits somewhere, so that they can be freed up if the deposit
+	// amount changes.
+	pub struct CommitmentDepositCollector;
+
+	impl StorageDepositCollector<AccountId, (AccountId, IdentityCommitmentVersion), RuntimeHoldReason>
+		for CommitmentDepositCollector
+	{
+		type Currency = Balances;
+		type Reason = HoldReason;
+
+		fn reason() -> Self::Reason {
+			Self::Reason::Deposit
+		}
+
+		fn deposit(
+			key: &(AccountId, IdentityCommitmentVersion),
+		) -> Result<Deposit<AccountId, <Self::Currency as Inspect<AccountId>>::Balance>, DispatchError> {
+			log::error!("CommitmentDepositCollector::deposit(key) called, when it should not have, since it returns a dummy value.");
+			Err(CommitmentDepositCollectorError::Internal.into())
+		}
+
+		fn deposit_amount(
+			key: &(AccountId, IdentityCommitmentVersion),
+		) -> <Self::Currency as Inspect<AccountId>>::Balance {
+			DEPOSIT
+		}
+
+		fn get_hashed_key(key: &(AccountId, IdentityCommitmentVersion)) -> Result<Vec<u8>, DispatchError> {
+			log::error!("CommitmentDepositCollector::get_hashed_key(key) called, when it should not have, since it returns a dummy value.");
+			Err(CommitmentDepositCollectorError::Internal.into())
+		}
+
+		fn store_deposit(
+			key: &(AccountId, IdentityCommitmentVersion),
+			deposit: Deposit<AccountId, <Self::Currency as Inspect<AccountId>>::Balance>,
+		) -> Result<(), DispatchError> {
+			log::error!("CommitmentDepositCollector::get_hashed_key(key) called, when it should not have, since it returns a dummy value.");
+			Err(CommitmentDepositCollectorError::Internal.into())
+		}
+	}
+
+	impl ProviderHooks for CommitmentDepositCollector {
+		type Error = CommitmentDepositCollectorError;
+		type Identifier = DidIdentifier;
+		type IdentityCommitment = Hash;
+		type Submitter = AccountId;
+		type Success = ();
+
+		fn on_identity_committed(
+			identifier: &Self::Identifier,
+			submitter: &Self::Submitter,
+			commitment: &Self::IdentityCommitment,
+			version: IdentityCommitmentVersion,
+		) -> Result<Self::Success, Self::Error> {
+			let _deposit = Self::create_deposit(submitter, DEPOSIT);
+			// TODO: Store deposit somewhere
+			Ok(())
+		}
+
+		fn on_commitment_removed(
+			identifier: &Self::Identifier,
+			submitter: &Self::Submitter,
+			commitment: &Self::IdentityCommitment,
+			version: IdentityCommitmentVersion,
+		) -> Result<Self::Success, Self::Error> {
+			let deposit = Self::deposit((submitter, version))?;
+			Self::free_deposit(deposit)
+		}
+	}
+
+	// impl<Currency, HoldReason, const DEPOSIT_AMOUNT: Balance> ProviderHooks
+	// 	for CommitmentDepositCollector<Currency, HoldReason, DEPOSIT_AMOUNT>
+	// where
+	// 	Currency: MutateHold<AccountId, Reason = HoldReason>,
+	// 	HoldReason: Default,
+	// {
+	// 	type Error = CommitmentDepositCollector;
+	// 	type Identifier = DidIdentifier;
+	// 	type IdentityCommitment = Hash;
+	// 	type Submitter = AccountId;
+	// 	type Success = ();
+
+	// 	fn on_identity_committed(
+	// 		identifier: &Self::Identifier,
+	// 		submitter: &Self::Submitter,
+	// 		commitment: &Self::IdentityCommitment,
+	// 		version: IdentityCommitmentVersion,
+	// 	) -> Result<Self::Success, Self::Error> {
+	// 		let reason = HoldReason::default();
+	// 		reserve_deposit
+	// 	}
+	// }
 }
 
 impl pallet_dip_provider::Config for Runtime {
@@ -54,5 +189,7 @@ impl pallet_dip_provider::Config for Runtime {
 	type IdentityCommitmentGeneratorError = DidMerkleProofError;
 	type IdentityProvider = LinkedDidInfoProviderOf<Runtime>;
 	type IdentityProviderError = <LinkedDidInfoProviderOf<Runtime> as IdentityProvider<DidIdentifier>>::Error;
+	// TODO: Change to deposit collector
+	type ProviderHooks = NoopHooks<Self::Identifier, Self::IdentityCommitment, Self::AccountId>;
 	type RuntimeEvent = RuntimeEvent;
 }
--- dip-template/runtimes/dip-provider/src/lib.rs
+++ dip-template/runtimes/dip-provider/src/lib.rs
@@ -568,14 +568,14 @@ impl_runtime_apis! {
 		}
 	}
 
-	impl kilt_runtime_api_dip_provider::DipProvider<Block, RuntimeApiDipProofRequest, CompleteMerkleProof<Hash, DidMerkleProofOf<Runtime>>, RuntimeApiDipProofError> for Runtime {
-		fn generate_proof(request: RuntimeApiDipProofRequest) -> Result<CompleteMerkleProof<Hash, DidMerkleProofOf<Runtime>>, RuntimeApiDipProofError> {
+	impl kilt_runtime_api_dip_provider::DipProvider<Block, runtime_api::DipProofRequest, CompleteMerkleProof<Hash, DidMerkleProofOf<Runtime>>, runtime_api::DipProofError> for Runtime {
+		fn generate_proof(request: runtime_api::DipProofRequest) -> Result<CompleteMerkleProof<Hash, DidMerkleProofOf<Runtime>>, runtime_api::DipProofError> {
 			let linked_did_info = match <Runtime as pallet_dip_provider::Config>::IdentityProvider::retrieve(&request.identifier) {
 				Ok(Some(linked_did_info)) => Ok(linked_did_info),
-				Ok(None) => Err(RuntimeApiDipProofError::IdentityNotFound),
-				Err(e) => Err(RuntimeApiDipProofError::IdentityProviderError(e))
+				Ok(None) => Err(runtime_api::DipProofError::IdentityNotFound),
+				Err(e) => Err(runtime_api::DipProofError::IdentityProviderError(e))
 			}?;
-			DidMerkleRootGenerator::<Runtime>::generate_proof(&linked_did_info, request.version, request.keys.iter(), request.should_include_web3_name, request.accounts.iter()).map_err(RuntimeApiDipProofError::MerkleProofError)
+			DidMerkleRootGenerator::<Runtime>::generate_proof(&linked_did_info, request.version, request.keys.iter(), request.should_include_web3_name, request.accounts.iter()).map_err(runtime_api::DipProofError::MerkleProofError)
 		}
 	}
 }
