5e8bcf40750cfda5b202ec648c1dbb393a7c65a7
Antonio Antonino <antonio@kilt.io>
2023-12-12 16:07:25 +0100

    Add Docker image push for DIP provider and consumer templates

--- .maintain/push-image.sh
+++ .maintain/push-image.sh
@@ -6,13 +6,21 @@ target_tag=$2
 # publish to docker hub
 docker tag local/standalone-node:$source_tag ${DOCKER_HUB_STANDALONE}:$target_tag
 docker tag local/kilt-node:$source_tag ${DOCKER_HUB_PARACHAIN}:$target_tag
+docker tag local/dip-provider-node-template:$source_tag ${DOCKER_HUB_DIP_PROVIDER_TEMPLATE}:$target_tag
+docker tag local/dip-consumer-node-template:$source_tag ${DOCKER_HUB_DIP_CONSUMER_TEMPLATE}:$target_tag
 
 docker push ${DOCKER_HUB_STANDALONE}:$target_tag
 docker push ${DOCKER_HUB_PARACHAIN}:$target_tag
+docker push ${DOCKER_HUB_DIP_PROVIDER_TEMPLATE}:$target_tag
+docker push ${DOCKER_HUB_DIP_CONSUMER_TEMPLATE}:$target_tag
 
 # publish to AWS
 docker tag local/standalone-node:$source_tag $AWS_REGISTRY/kilt/prototype-chain:$target_tag
 docker tag local/kilt-node:$source_tag $AWS_REGISTRY/kilt-parachain/collator:$target_tag
+docker tag local/dip-provider-node-template:$source_tag $AWS_REGISTRY/dip-provider-node-template:$target_tag
+docker tag local/dip-consumer-node-template:$source_tag $AWS_REGISTRY/dip-consumer-node-template:$target_tag
 
 docker push $AWS_REGISTRY/kilt/prototype-chain:$target_tag
 docker push $AWS_REGISTRY/kilt-parachain/collator:$target_tag
+docker push $AWS_REGISTRY/dip-provider-node-template:$target_tag
+docker push $AWS_REGISTRY/dip-consumer-node-template:$target_tag
