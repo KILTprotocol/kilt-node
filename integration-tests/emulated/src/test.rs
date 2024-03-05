578ce31aa41105ee4d89896f24e6bd45955fd7ab
Adel Golghalyani <ad96el@gmail.com>
2024-03-05 08:33:18 +0100

    intermediate state

--- integration-tests/emulated/src/test.rs
+++ integration-tests/emulated/src/test.rs
@@ -2,7 +2,7 @@ use crate::{
 	mock::{
 		network::MockNetwork,
 		para_chains::{AssetHub, Spiritnet},
-		relay_chains::Polkadot,
+		relay_chains::{Polkadot, PolkadotPallet},
 	},
 	utils::get_account_id_from_seed,
 };
@@ -10,15 +10,20 @@ use asset_hub_polkadot_runtime::{RuntimeEvent as AssetHubRuntimeEvent, System as
 use frame_support::dispatch::RawOrigin;
 use frame_support::{assert_err, assert_ok};
 use integration_tests_common::{asset_hub_polkadot, polkadot::ED, ALICE, BOB};
+use parity_scale_codec::Encode;
 use polkadot_runtime::System as PolkadotSystem;
 use sp_core::sr25519;
 use sp_runtime::{DispatchError, ModuleError};
 use spiritnet_runtime::{
 	PolkadotXcm as SpiritnetXcm, RuntimeEvent as SpiritnetRuntimeEvent, System as SpiritnetSystem,
 };
-use xcm::v3::WeightLimit;
+use xcm::{v3::WeightLimit, DoubleEncoded, VersionedMultiLocation, VersionedXcm};
 use xcm_emulator::{
-	cumulus_pallet_xcmp_queue::Event as XcmpQueueEvent, Here, Junction, Junctions, Parent, ParentThen, TestExt, X1,
+	assert_expected_events,
+	cumulus_pallet_xcmp_queue::Event as XcmpQueueEvent,
+	Here,
+	Instruction::{Transact, UnpaidExecution},
+	Junction, Junctions, OriginKind, Parachain, Parent, ParentThen, RelayChain, TestExt, Weight, Xcm, X1,
 };
 
 #[test]
@@ -155,6 +160,65 @@ fn test_teleport_asset_from_regular_account_to_asset_hub() {
 	AssetHub::execute_with(|| {
 		assert_eq!(AssetHubSystem::events().len(), 0);
 	});
+}
+
+#[test]
+fn test_sudo_call_from_relay_chain_to_spiritnet() {
+	let code = vec![];
+
+	let call: DoubleEncoded<()> = <Spiritnet as Parachain>::RuntimeCall::System(frame_system::Call::set_code { code })
+		.encode()
+		.into();
+	let sudo_origin = <Polkadot as RelayChain>::RuntimeOrigin::root();
+	let parachain_destination: VersionedMultiLocation = Polkadot::child_location_of(Spiritnet::para_id()).into();
+
+	let weight_limit = WeightLimit::Unlimited;
+	let require_weight_at_most = Weight::from_parts(1000000000, 200000);
+	let origin_kind = OriginKind::Superuser;
+	let check_origin = None;
+
+	let xcm = VersionedXcm::from(Xcm(vec![
+		UnpaidExecution {
+			weight_limit,
+			check_origin,
+		},
+		Transact {
+			origin_kind,
+			require_weight_at_most,
+			call,
+		},
+	]));
+
+	//Send XCM message from relay chain
+
+	Polkadot::execute_with(|| {
+		assert_ok!(<Polkadot as PolkadotPallet>::XcmPallet::send(
+			sudo_origin,
+			Box::new(parachain_destination),
+			Box::new(xcm)
+		));
+
+		type RuntimeEvent = <Polkadot as RelayChain>::RuntimeEvent;
+
+		assert_expected_events!(
+			Polkadot,
+			vec![
+				RuntimeEvent::XcmPallet(pallet_xcm::Event::Sent { .. }) => {},
+			]
+		);
+	});
+
+	Spiritnet::execute_with(|| {
+		assert!(matches!(
+			SpiritnetSystem::events()
+				.first()
+				.expect("An event should be emitted when sending an XCM message.")
+				.event,
+			SpiritnetRuntimeEvent::PolkadotXcm(pallet_xcm::Event::Attempted {
+				outcome: xcm::latest::Outcome::Error(xcm::latest::Error::Barrier)
+			})
+		));
+	})
 }
 
 // TODO: Receive funds from assetHub
