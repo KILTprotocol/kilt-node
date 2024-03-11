67651cebbf8623a571c4a372c92d62ca16e503ae
Adel Golghalyani <ad96el@gmail.com>
2024-03-11 16:46:14 +0100

    clean up reserve transfers peregrine

--- integration-tests/emulated/src/tests/peregrine/reserve_transfers.rs
+++ integration-tests/emulated/src/tests/peregrine/reserve_transfers.rs
@@ -10,7 +10,6 @@ use crate::{
 use frame_support::{assert_ok, dispatch::RawOrigin, traits::fungible::Inspect};
 use integration_tests_common::{asset_hub_polkadot, polkadot::ED, ALICE};
 use peregrine_runtime::PolkadotXcm as PeregrineXcm;
-use rococo_runtime::System as RococoSystem;
 use runtime_common::AccountId;
 use sp_core::sr25519;
 use sp_runtime::traits::Zero;
@@ -55,7 +54,7 @@ fn test_reserve_asset_transfer_from_regular_peregrine_account_to_relay() {
 	});
 	// No message should reach the relaychain.
 	Rococo::execute_with(|| {
-		assert_eq!(RococoSystem::events().len(), 0);
+		assert_eq!(Rococo::events().len(), 0);
 	})
 }
 
@@ -112,7 +111,7 @@ fn test_reserve_asset_transfer_from_regular_peregrine_account_to_asset_hub() {
 	});
 	// No event on the relaychain (message is meant for AssetHub.
 	Rococo::execute_with(|| {
-		assert_eq!(RococoSystem::events().len(), 0);
+		assert_eq!(Rococo::events().len(), 0);
 	});
 	// Fails on AssetHub since peregrine is not a trusted registrar
 	AssetHubRococo::execute_with(|| {
