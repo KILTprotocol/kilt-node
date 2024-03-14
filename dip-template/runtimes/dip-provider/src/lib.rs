b28a400bd9dfbf8a2007ce21c8473fa20627935e
Adel Golghalyani <48685760+Ad96el@users.noreply.github.com>
2024-03-14 10:13:32 +0100

    chore: prepare new dev version (#579)
    ## fixes new Dev version

--- dip-template/runtimes/dip-provider/src/lib.rs
+++ dip-template/runtimes/dip-provider/src/lib.rs
@@ -156,7 +156,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("dip-provider-runtime-template"),
 	impl_name: create_runtime_str!("dip-provider-runtime-template"),
 	authoring_version: 1,
-	spec_version: 11100,
+	spec_version: 11300,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
