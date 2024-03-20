e35867c8fb1e51aa689a1489f7ebb610a8344df8
Adel Golghalyani <ad96el@gmail.com>
2024-03-20 11:22:29 +0100

    test unpaid execution from relay

--- integration-tests/emulated/src/tests/peregrine/unpaid_execution.rs
+++ integration-tests/emulated/src/tests/peregrine/unpaid_execution.rs
@@ -1,16 +1,17 @@
 use crate::mock::{
 	network::MockNetworkRococo,
 	para_chains::{peregrine, AssetHubRococo, AssetHubRococoPallet, Peregrine},
-	relay_chains::Rococo,
+	relay_chains::{Rococo, RococoPallet},
 };
 use frame_support::assert_ok;
 use xcm::{v3::WeightLimit, VersionedMultiLocation, VersionedXcm};
 use xcm_emulator::{
-	assert_expected_events, Instruction::UnpaidExecution, Junction, Junctions, Parachain, ParentThen, TestExt, Xcm,
+	assert_expected_events, BodyId, BodyPart, Instruction::UnpaidExecution, Junction, Junctions, MultiLocation,
+	Outcome, Parachain, ParentThen, Plurality, RelayChain, TestExt, Xcm, X1,
 };
 
 #[test]
-fn test_unpaid_execution_to_peregrine() {
+fn test_unpaid_execution_from_asset_hub_to_peregrine() {
 	MockNetworkRococo::reset();
 
 	let sudo_origin = <AssetHubRococo as Parachain>::RuntimeOrigin::root();
@@ -59,5 +60,63 @@ fn test_unpaid_execution_to_peregrine() {
 	// No event on Rococo. message is meant for Peregrine
 	Rococo::execute_with(|| {
 		assert_eq!(Rococo::events().len(), 0);
+	});
+}
+
+#[test]
+fn test_unpaid_execution_from_rococo_to_peregrine() {
+	MockNetworkRococo::reset();
+
+	let sudo_origin = <Rococo as RelayChain>::RuntimeOrigin::root();
+	let parachain_destination: VersionedMultiLocation = Junctions::X1(Junction::Parachain(peregrine::PARA_ID)).into();
+
+	let weight_limit = WeightLimit::Unlimited;
+	let check_origin = Some(MultiLocation {
+		parents: 1,
+		interior: X1(Plurality {
+			id: BodyId::Legislative,
+			part: BodyPart::Voice,
+		}),
+	});
+
+	let xcm = VersionedXcm::from(Xcm(vec![UnpaidExecution {
+		weight_limit,
+		check_origin,
+	}]));
+
+	//Send XCM message from relay chain
+	Rococo::execute_with(|| {
+		assert_ok!(<Rococo as RococoPallet>::XcmPallet::send(
+			sudo_origin,
+			Box::new(parachain_destination),
+			Box::new(xcm)
+		));
+
+		type RuntimeEvent = <Rococo as RelayChain>::RuntimeEvent;
+
+		assert_expected_events!(
+			Rococo,
+			vec![
+				RuntimeEvent::XcmPallet(pallet_xcm::Event::Sent { .. }) => {},
+			]
+		);
+	});
+
+	Peregrine::execute_with(|| {
+		type PeregrineRuntimeEvent = <Peregrine as Parachain>::RuntimeEvent;
+		assert_expected_events!(
+			Peregrine,
+			vec![
+				PeregrineRuntimeEvent::DmpQueue(cumulus_pallet_dmp_queue::Event::ExecutedDownward {
+					outcome: Outcome::Error(xcm::v3::Error::Barrier),
+					..
+				}) => {},
+			]
+		);
+	});
+
+	// No event on AssetHubRococo. message is meant for Peregrine
+	AssetHubRococo::execute_with(|| {
+		assert_eq!(AssetHubRococo::events().len(), 0);
 	});
 }
