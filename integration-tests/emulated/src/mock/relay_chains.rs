4377d493fc4e1e5c5c47f5cf0d32f97f8f42f945
Antonio <antonio@kilt.io>
2024-03-31 12:00:56 +0200

    feat: open up XCM configurations (#603)
    # Fixes https://github.com/KILTprotocol/ticket/issues/3082.
    
    
    The following PR enables reserve transfers to other chains. Important
    DID pallets are whitelisted to authorize these extrinsics for execution
    within the transact instruction. Validation is required to ensure the
    correctness of the whitelisting, as the whitelisted call should not
    trigger another dispatch call.
    
     
    ---------
    
    Co-authored-by: Albrecht <albrecht@kilt.io>
    Co-authored-by: Adel Golghalyani <ad96el@gmail.com>
    Co-authored-by: Antonio Antoninio <antonio@kilt.io>

---
+++ integration-tests/emulated/src/mock/relay_chains.rs
@@ -0,0 +1,59 @@
+// KILT Blockchain – https://botlabs.org
+// Copyright (C) 2019-2024 BOTLabs GmbH
+
+// The KILT Blockchain is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// The KILT Blockchain is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+use integration_tests_common::{polkadot, rococo};
+use xcm_emulator::{decl_test_relay_chains, RelayChain, TestExt, XcmHash};
+
+decl_test_relay_chains! {
+	#[api_version(5)]
+	pub struct Polkadot {
+		genesis = polkadot::genesis(),
+		on_init = (),
+		runtime = {
+			Runtime: polkadot_runtime::Runtime,
+			RuntimeOrigin: polkadot_runtime::RuntimeOrigin,
+			RuntimeCall: polkadot_runtime::RuntimeCall,
+			RuntimeEvent: polkadot_runtime::RuntimeEvent,
+			MessageQueue: polkadot_runtime::MessageQueue,
+			XcmConfig: polkadot_runtime::xcm_config::XcmConfig,
+			SovereignAccountOf: polkadot_runtime::xcm_config::SovereignAccountOf,
+			System: polkadot_runtime::System,
+			Balances: polkadot_runtime::Balances,
+		},
+		pallets_extra = {
+			XcmPallet: polkadot_runtime::XcmPallet,
+		}
+	},
+	#[api_version(5)]
+	pub struct Rococo {
+		genesis = rococo::genesis(),
+		on_init = (),
+		runtime = {
+			Runtime: rococo_runtime::Runtime,
+			RuntimeOrigin: rococo_runtime::RuntimeOrigin,
+			RuntimeCall: rococo_runtime::RuntimeCall,
+			RuntimeEvent: rococo_runtime::RuntimeEvent,
+			MessageQueue: rococo_runtime::MessageQueue,
+			XcmConfig: rococo_runtime::xcm_config::XcmConfig,
+			SovereignAccountOf: rococo_runtime::xcm_config::LocationConverter,
+			System: rococo_runtime::System,
+			Balances: rococo_runtime::Balances,
+		},
+		pallets_extra = {
+			XcmPallet: rococo_runtime::XcmPallet,
+		}
+	}
+}
---
+++ integration-tests/emulated/src/tests/peregrine/teleportation.rs
@@ -0,0 +1,70 @@
+// KILT Blockchain – https://botlabs.org
+// Copyright (C) 2019-2024 BOTLabs GmbH
+
+// The KILT Blockchain is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// The KILT Blockchain is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+// If you feel like getting in touch with us, you can do so at info@botlabs.org
+
+use frame_support::assert_noop;
+use frame_support::dispatch::RawOrigin;
+use integration_tests_common::{asset_hub_polkadot, polkadot::ED, ALICE, BOB};
+use peregrine_runtime::PolkadotXcm as PeregrineXcm;
+use sp_core::sr25519;
+use xcm::v3::WeightLimit;
+use xcm_emulator::{Here, Junction, Junctions, ParentThen, TestExt, X1};
+
+use crate::{
+	mock::{
+		network::MockNetworkRococo,
+		para_chains::{AssetHubRococo, Peregrine},
+		relay_chains::Rococo,
+	},
+	utils::get_account_id_from_seed,
+};
+
+#[test]
+fn test_teleport_asset_from_regular_peregrine_account_to_asset_hub() {
+	MockNetworkRococo::reset();
+
+	let alice_account_id = get_account_id_from_seed::<sr25519::Public>(ALICE);
+	let bob_account_id = get_account_id_from_seed::<sr25519::Public>(BOB);
+
+	Peregrine::execute_with(|| {
+		assert_noop!(
+			PeregrineXcm::limited_teleport_assets(
+				RawOrigin::Signed(alice_account_id.clone()).into(),
+				Box::new(ParentThen(Junctions::X1(Junction::Parachain(asset_hub_polkadot::PARA_ID))).into()),
+				Box::new(
+					X1(Junction::AccountId32 {
+						network: None,
+						id: bob_account_id.into()
+					})
+					.into()
+				),
+				Box::new((Here, 1000 * ED).into()),
+				0,
+				WeightLimit::Unlimited,
+			),
+			pallet_xcm::Error::<peregrine_runtime::Runtime>::Filtered
+		);
+	});
+	// No event on the relaychain Message is for AssetHub
+	Rococo::execute_with(|| {
+		assert_eq!(Rococo::events().len(), 0);
+	});
+	// AssetHub should not receive any message, since the message is filtered out.
+	AssetHubRococo::execute_with(|| {
+		assert_eq!(AssetHubRococo::events().len(), 0);
+	});
+}
