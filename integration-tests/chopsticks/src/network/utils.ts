a2d63930d82680997ef4ad42a6809a72e4acc9cf
Adel Golghalyani <ad96el@gmail.com>
2024-04-15 10:21:42 +0200

    remove version

--- integration-tests/chopsticks/src/network/utils.ts
+++ integration-tests/chopsticks/src/network/utils.ts
@@ -1,72 +1,40 @@
 import { u8aToHex } from '@polkadot/util'
 import { decodeAddress } from '@polkadot/util-crypto'
 
-export function getSiblingAccountDestinationV3(paraId: number, addr: string) {
+export function getSiblingAccountDestination(paraId: number, addr: string) {
 	return {
-		V3: {
-			parents: 1,
-			interior: {
-				X2: [
-					{ Parachain: paraId },
-					{
-						AccountId32: {
-							id: u8aToHex(decodeAddress(addr)),
-						},
+		parents: 1,
+		interior: {
+			X2: [
+				{ Parachain: paraId },
+				{
+					AccountId32: {
+						id: u8aToHex(decodeAddress(addr)),
 					},
-				],
-			},
+				},
+			],
 		},
 	}
 }
 
-export function getSiblingAccountDestinationV2(paraId: number, addr: string) {
+export function getParentAccountDestination(addr: string) {
 	return {
-		V2: {
-			parents: 1,
-			interior: {
-				X2: [
-					{ Parachain: paraId },
-					{
-						AccountId32: {
-							id: u8aToHex(decodeAddress(addr)),
-						},
+		parents: 1,
+		interior: {
+			X1: [
+				{
+					AccountId32: {
+						id: u8aToHex(decodeAddress(addr)),
 					},
-				],
-			},
+				},
+			],
 		},
 	}
 }
 
-export function getParentAccountDestinationV3(addr: string) {
+export function getNativeAssetIdLocation(amount: bigint) {
 	return {
-		V3: {
-			parents: 1,
-			interior: {
-				X1: [
-					{
-						AccountId32: {
-							id: u8aToHex(decodeAddress(addr)),
-						},
-					},
-				],
-			},
-		},
-	}
-}
-
-export function getParentAccountDestinationV2(addr: string) {
-	return {
-		V2: {
-			parents: 1,
-			interior: {
-				X1: [
-					{
-						AccountId32: {
-							id: u8aToHex(decodeAddress(addr)),
-						},
-					},
-				],
-			},
-		},
+		id: { Concrete: { parents: 0, interior: 'Here' } },
+		fun: { Fungible: amount },
 	}
 }
