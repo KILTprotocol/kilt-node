3bc284ab8e5243bdde3ac3e493f98742ea55ecf6
Adel Golghalyani <ad96el@gmail.com>
2024-05-27 16:00:24 +0200

    add register

--- runtimes/kestrel/src/lib.rs
+++ runtimes/kestrel/src/lib.rs
@@ -1178,3 +1178,8 @@ impl_runtime_apis! {
 		}
 	}
 }
+
+cumulus_pallet_parachain_system::register_validate_block! {
+	Runtime = Runtime,
+	BlockExecutor = cumulus_pallet_aura_ext::BlockExecutor::<Runtime, Executive>,
+}
