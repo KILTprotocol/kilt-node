ede506607e35d70a5a382815d8d4a45d3fd4ddde
Adel Golghalyani <ad96el@gmail.com>
2024-06-10 13:00:58 +0200

    resolve clippy comments

--- runtimes/peregrine/src/weights/mod.rs
+++ runtimes/peregrine/src/weights/mod.rs
@@ -15,6 +15,7 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 // If you feel like getting in touch with us, you can do so at info@botlabs.org
+#![allow(unused_doc_comments)]
 
 pub mod attestation;
 pub mod ctype;
