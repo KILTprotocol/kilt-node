731a60fb0ee6a1e3f59b20d6c80848334802d014
Antonio Antonino <antonio@kilt.io>
2024-06-21 09:42:55 +0200

    Fix up implementation of IsReserve

--- pallets/pallet-asset-swap/src/xcm/reserve.rs
+++ pallets/pallet-asset-swap/src/xcm/reserve.rs
@@ -22,10 +22,9 @@ use xcm::prelude::{AssetId, MultiAsset, MultiLocation};
 
 use crate::{Config, SwapPair, LOG_TARGET};
 
-// TODO: Add unit tests
-pub struct AllowOnlySwapAssetFromConfiguredLocation<T>(PhantomData<T>);
+pub struct ReserveTransfersOfXcmFeeAssetAndRemoteAsset<T>(PhantomData<T>);
 
-impl<T> ContainsPair<MultiAsset, MultiLocation> for AllowOnlySwapAssetFromConfiguredLocation<T>
+impl<T> ContainsPair<MultiAsset, MultiLocation> for ReserveTransfersOfXcmFeeAssetAndRemoteAsset<T>
 where
 	T: Config,
 {
@@ -36,7 +35,8 @@ where
 			return false;
 		};
 
-		// 2. Verify the expected reserve location matches exactly the XCM origin
+		// 2. For both XCM fee asset and remote asset, we only trust the configured
+		//    remote location.
 		let Ok(stored_remote_reserve_location_as_required_version): Result<MultiLocation, _> = swap_pair.remote_reserve_location.clone().try_into().map_err(|e| {
 			log::error!(target: LOG_TARGET, "Failed to convert stored remote reserve location {:?} into required version with error {:?}.", swap_pair.remote_reserve_location, e);
 			e
@@ -51,8 +51,8 @@ where
 			return false;
 		}
 
-		// 3. Verify the asset matches the expected one from the other side of the swap
-		//    pair
+		// 3. Verify the asset matches either the configured XCM fee asset or remote
+		//    asset to swap for local ones.
 		let Ok(stored_remote_asset_id): Result<AssetId, _> = swap_pair.remote_asset_id.clone().try_into().map_err(|e| {
 			log::error!(target: LOG_TARGET, "Failed to convert stored remote asset ID {:?} into required version with error {:?}.", swap_pair.remote_asset_id, e);
 			e
@@ -66,43 +66,6 @@ where
 			);
 			return false;
 		}
-
-		true
-	}
-}
-
-// TODO: Could all be unified into one so that we access storage only once,
-// although with caching this should not really be an issue.
-// TODO: Add unit tests
-pub struct AllowOnlyXcmFeeAssetFromConfiguredLocation<T>(PhantomData<T>);
-
-impl<T> ContainsPair<MultiAsset, MultiLocation> for AllowOnlyXcmFeeAssetFromConfiguredLocation<T>
-where
-	T: Config,
-{
-	fn contains(a: &MultiAsset, b: &MultiLocation) -> bool {
-		// 1. Verify a swap pair has been set.
-		let Some(swap_pair) = SwapPair::<T>::get() else {
-			log::trace!(target: LOG_TARGET, "No swap pair configured.");
-			return false;
-		};
-
-		// 2. Verify the expected reserve location matches exactly the XCM origin
-		let Ok(stored_remote_reserve_location_as_required_version): Result<MultiLocation, _> = swap_pair.remote_reserve_location.clone().try_into().map_err(|e| {
-			log::error!(target: LOG_TARGET, "Failed to convert stored remote reserve location {:?} into required version with error {:?}.", swap_pair.remote_reserve_location, e);
-			e
-		 }) else { return false; };
-		if stored_remote_reserve_location_as_required_version != *b {
-			log::trace!(
-				target: LOG_TARGET,
-				"Remote origin {:?} does not match expected origin {:?}",
-				b,
-				stored_remote_reserve_location_as_required_version
-			);
-			return false;
-		}
-
-		// 3. Verify the asset matches the one configured as fee asset.
 		let Ok(stored_remote_asset_fee): Result<MultiAsset, _> = swap_pair.remote_fee.clone().try_into().map_err(|e| {
 			log::error!(target: LOG_TARGET, "Failed to convert stored remote asset fee {:?} into required version with error {:?}.", swap_pair.remote_fee, e);
 			e
@@ -117,6 +80,13 @@ where
 			return false;
 		}
 
-		true
+		match a.id {
+			remote_asset_id if remote_asset_id == stored_remote_asset_id => true,
+			xcm_fee_asset_id if xcm_fee_asset_id == stored_remote_asset_fee.id => true,
+			_ => {
+				log::info!(target: LOG_TARGET, "Received asset ID {:?} does not match neither the expected remote asset ID {:?} nor the XCM fee asset ID {:?}", a.id, stored_remote_asset_id, stored_remote_asset_fee.id);
+				false
+			}
+		}
 	}
 }
