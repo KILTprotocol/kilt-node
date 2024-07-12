91faf3400b0a366d40608b3be36a4bc5aed1e292
Adel Golghalyani <ad96el@gmail.com>
2024-07-12 13:38:22 +0200

    limited reserve transfer spiritnet 2 ah

---
+++ integration-tests/chopsticks/src/network/assetHub.ts
@@ -0,0 +1,68 @@
+import { setupContext, SetupOption } from '@acala-network/chopsticks-testing'
+
+import type { Config } from './types.js'
+import { initialBalanceDOT, toNumber } from '../utils.js'
+
+/// Options used to create the Spiritnet context
+export const getSetupOptions = ({
+	blockNumber = undefined,
+	wasmOverride = undefined,
+}: {
+	blockNumber?: number
+	wasmOverride?: string
+}) =>
+	({
+		endpoint: process.env.ASSETHUB_WSS || 'wss://asset-hub-polkadot-rpc.dwellir.com',
+		db: './db/assethub.db.sqlite',
+		port: toNumber(process.env.ASSETHUB_PORT) || 9003,
+		wasmOverride,
+		blockNumber,
+	}) as SetupOption
+
+export function assigneKSMtoAccounts(addr: string[], balance: bigint = initialBalanceDOT) {
+	return {
+		foreignAssets: {
+			account: addr.map((addr) => [
+				[
+					{
+						parents: 2,
+						interior: {
+							X1: [
+								{
+									GlobalConsensus: 'Kusama',
+								},
+							],
+						},
+					},
+					addr,
+				],
+				{
+					balance: balance,
+					status: 'Liquid',
+					reason: 'Consumer',
+					extra: null,
+				},
+			]),
+		},
+	}
+}
+
+/// AssetHub has no own coin. Teleported dots are used as the native token.
+export function assignDotTokensToAccounts(addr: string[], balance: bigint) {
+	return {
+		System: {
+			Account: addr.map((address) => [[address], { providers: 1, data: { free: balance.toString() } }]),
+		},
+	}
+}
+
+/// AssetHub ParaId
+export const paraId = 1000
+
+// Sibling Sovereign Account
+export const siblingSovereignAccount = '4qXPdpimHh8TR24RSk994yVzxx4TLfvKj5i1qH5puvWmfAqy'
+
+export async function getContext(): Promise<Config> {
+	const options = getSetupOptions({})
+	return setupContext(options)
+}
---
+++ integration-tests/chopsticks/src/tests/xcm/assetHub/limitedReserveTransferSpiritnetAssetHub.test.ts
@@ -0,0 +1,57 @@
+import { test } from 'vitest'
+import { sendTransaction, withExpect } from '@acala-network/chopsticks-testing'
+
+import * as SpiritnetConfig from '../../../network/spiritnet.js'
+import * as AssetHubConfig from '../../../network/assetHub.js'
+import { KILT, initialBalanceKILT, keysAlice } from '../../../utils.js'
+import { spiritnetContext, assetHubContext, getFreeBalanceSpiritnet } from '../../index.js'
+import { getAccountLocationV3, getNativeAssetIdLocation, getSiblingLocation } from '../../../network/utils.js'
+import { checkBalance, createBlock, hexAddress, setStorage } from '../../utils.js'
+
+test('Limited Reserve Transfers from Spiritnet Account Alice -> AH Account Alice', async ({ expect }) => {
+	const { checkEvents, checkSystemEvents } = withExpect(expect)
+
+	// Assign alice some KILT tokens
+	await setStorage(spiritnetContext, {
+		...SpiritnetConfig.assignNativeTokensToAccounts([keysAlice.address], initialBalanceKILT),
+		...SpiritnetConfig.setSafeXcmVersion3(),
+	})
+
+	// Balance of the hydraDx sovereign account before the transfer
+	const assetHubSovereignAccountBalance = await getFreeBalanceSpiritnet(AssetHubConfig.siblingSovereignAccount)
+
+	// check initial balance of Alice on Spiritnet
+	await checkBalance(getFreeBalanceSpiritnet, keysAlice.address, expect, initialBalanceKILT)
+
+	const aliceAddress = hexAddress(keysAlice.address)
+	const assetHubDestination = { V3: getSiblingLocation(AssetHubConfig.paraId) }
+	const beneficiary = getAccountLocationV3(aliceAddress)
+	const kiltAsset = { V3: [getNativeAssetIdLocation(KILT)] }
+
+	const signedTx = spiritnetContext.api.tx.polkadotXcm
+		.limitedReserveTransferAssets(assetHubDestination, beneficiary, kiltAsset, 0, 'Unlimited')
+		.signAsync(keysAlice)
+
+	const events = await sendTransaction(signedTx)
+
+	// Check sender state
+	await createBlock(spiritnetContext)
+
+	// Check events sender
+	checkEvents(events, 'xcmpQueue').toMatchSnapshot('sender events xcm queue pallet')
+	checkEvents(events, 'polkadotXcm').toMatchSnapshot('sender events xcm pallet')
+	checkEvents(events, { section: 'balances', method: 'Withdraw' }).toMatchSnapshot('sender events Balances')
+
+	//	check balance. The sovereign account should hold one additional KILT.
+	await checkBalance(
+		getFreeBalanceSpiritnet,
+		AssetHubConfig.siblingSovereignAccount,
+		expect,
+		assetHubSovereignAccountBalance + KILT
+	)
+
+	await createBlock(assetHubContext)
+
+	// MSG processing will fail on AH.
+	await checkSystemEvents(assetHubContext, 'messageQueue').toMatchSnapshot('AH message queue')
+}, 20_000)
