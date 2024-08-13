a4561c55a3a0a59ebff5ed7622c0e0a9c681c6de
Gerawork Aynekulu <ggera@users.noreply.github.com>
2024-08-13 14:12:37 +0200

    ci: caching (#657)
    ## fixes KILTProtocol/ticket#3345
    Breaks up the on `gitlab-ci.yml` & adds one more stage release.
    - [x] Migrate few jobs from gitlab to Github
    - [x]  Enable caching
    ## Metadata Diff to Develop Branch
    
    <details>
    <summary>Peregrine Diff</summary>
    
    ```
    ```
    
    </details>
    
    <details>
    <summary>Spiritnet Diff</summary>
    
    ```
    ```
    
    </details>
    
    ## Checklist:
    
    - [ ] I have verified that the code works
    - [ ] No panics! (checked arithmetic ops, no indexing `array[3]` use
    `get(3)`, ...)
    - [ ] I have verified that the code is easy to understand
      - [ ] If not, I have left a well-balanced amount of inline comments
    - [ ] I have [left the code in a better
    state](https://deviq.com/principles/boy-scout-rule)
    - [ ] I have documented the changes (where applicable)
    * Either PR or Ticket to update [the
    Docs](https://github.com/KILTprotocol/docs)
        * Link the PR/Ticket here
    
    ---------
    
    Co-authored-by: Chris Chinchilla <chris@kilt.io>

--- .maintain/build-image.sh
+++ .maintain/build-image.sh
@@ -1,40 +1,39 @@
 #!/bin/bash
 
+set -e
+
 target_tag=$1
 
+# Build the builder image and push it in the background
 docker build \
     --target builder \
     --cache-from $AWS_REGISTRY/kilt-parachain/collator:builder \
     -t $AWS_REGISTRY/kilt-parachain/collator:builder \
-    .
-docker push $AWS_REGISTRY/kilt-parachain/collator:builder
+    . &
+docker push $AWS_REGISTRY/kilt-parachain/collator:builder &
 
-# build parachain image and standalone image
-docker build \
-    --cache-from $AWS_REGISTRY/kilt-parachain/collator:builder \
-    --cache-from $AWS_REGISTRY/kilt-parachain/collator:$target_tag \
-    --build-arg NODE_TYPE=kilt-parachain \
-    -t local/kilt-node:$target_tag \
-    .
-docker build \
-    --cache-from $AWS_REGISTRY/kilt-parachain/collator:builder \
-    --cache-from $AWS_REGISTRY/kilt/prototype-chain:$target_tag \
-    --build-arg NODE_TYPE=standalone-node \
-    -t local/standalone-node:$target_tag \
-    .
+wait
 
-# build DIP provider and consumer templates
-PROVIDER_BIN_NAME="dip-provider-node-template"
-docker build \
-	--cache-from $AWS_REGISTRY/kilt-parachain/collator:builder \
-	--cache-from $AWS_REGISTRY/$PROVIDER_BIN_NAME:$target_tag \
-	--build-arg NODE_TYPE=$PROVIDER_BIN_NAME \
-	-t local/$PROVIDER_BIN_NAME:$target_tag \
-	.
-CONSUMER_BIN_NAME="dip-consumer-node-template"
-docker build \
-	--cache-from $AWS_REGISTRY/kilt-parachain/collator:builder \
-	--cache-from $AWS_REGISTRY/$CONSUMER_BIN_NAME:$target_tag \
-	--build-arg NODE_TYPE=$CONSUMER_BIN_NAME \
-	-t local/$CONSUMER_BIN_NAME:$target_tag \
-	.
+# Build and tag images in parallel
+build_and_tag() {
+    local node_type=$1
+    local image_name=$2
+    local cache_image=$3
+
+    docker build \
+        --cache-from $AWS_REGISTRY/kilt-parachain/collator:builder \
+        --cache-from $AWS_REGISTRY/$cache_image:$target_tag \
+        --build-arg NODE_TYPE=$node_type \
+        -t local/$image_name:$target_tag \
+        .
+}
+
+build_and_tag "kilt-parachain" "kilt-node" "kilt-parachain/collator" &
+
+build_and_tag "standalone-node" "standalone-node" "kilt/prototype-chain" &
+
+build_and_tag "dip-provider-node-template" "dip-provider-node-template" "kilt-parachain/collator" &
+
+build_and_tag "dip-consumer-node-template" "dip-consumer-node-template" "kilt-parachain/collator" &
+
+wait
