ff67ae12faf285bf10b1629b46ab9185f4681c42
Antonio <antonio@kilt.io>
2024-09-05 15:39:01 +0200

    feat: embed chainspecs in binary (#720)
    To avoid runtime panics. Fixes
    https://github.com/KILTprotocol/ticket/issues/3601 by embedding the
    well-known chainspecs into the binary. Fixes
    https://github.com/KILTprotocol/ticket/issues/3597 by defaulting to
    "peregrine" if a chainspec path is specified that does not contain
    neither `peregrine` nor `spiritnet`.

--- nodes/parachain/src/chain_spec/rilt/mod.rs
+++ nodes/parachain/src/chain_spec/rilt/mod.rs
@@ -25,7 +25,3 @@ const SAFE_XCM_VERSION: u32 = xcm::prelude::XCM_VERSION;
 /// Specialized `ChainSpec` for the normal parachain runtime.
 pub(crate) type ChainSpec =
 	sc_service::GenericChainSpec<peregrine_runtime::RuntimeGenesisConfig, crate::chain_spec::Extensions>;
-
-pub(crate) fn load_chain_spec(path: &str) -> Result<ChainSpec, String> {
-	ChainSpec::from_json_file(path.into())
-}
