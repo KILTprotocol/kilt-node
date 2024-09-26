d0f6ea9d987cdad7b4c2eec34685de02ac18b8dd
Adel Golghalyani <ad96el@gmail.com>
2024-09-26 13:19:59 +0200

    tests for square root function

--- pallets/pallet-bonded-coins/src/tests/types/curves/mod.rs
+++ pallets/pallet-bonded-coins/src/tests/types/curves/mod.rs
@@ -1,1 +1,9 @@
 mod polynomial_curve;
+mod square_root;
+
+use sp_runtime::FixedPointNumber;
+
+use crate::{mock::runtime::*, CurveParameterTypeOf};
+
+const NORMALIZED_DENOMINATION: u128 = CurveParameterTypeOf::<Test>::DIV;
+const CURRENT_DENOMINATION: u128 = 10u128.pow(15);
--- pallets/pallet-bonded-coins/src/tests/types/curves/square_root.rs
+++ pallets/pallet-bonded-coins/src/tests/types/curves/square_root.rs
@@ -1,8 +1,8 @@
 use sp_arithmetic::FixedU128;
-use sp_runtime::traits::Zero;
+use sp_runtime::{traits::Zero, FixedPointNumber};
 
 use crate::{
-	curves_parameters::{convert_currency_amount, PolynomialFunctionParameters},
+	curves_parameters::{convert_currency_amount, SquareRootBondingFunctionParameters},
 	mock::runtime::*,
 	tests::types::curves::{CURRENT_DENOMINATION, NORMALIZED_DENOMINATION},
 	types::{Curve, DiffKind},
@@ -10,11 +10,10 @@ use crate::{
 
 #[test]
 fn test_mint_first_coin() {
-	// Create curve with shape f(x) = 2x + 3, resulting into integral function F(x) = x^2 + 3x
-	let m = FixedU128::from_u32(0);
-	let n = FixedU128::from_u32(1);
-	let o = FixedU128::from_u32(3);
-	let curve = Curve::PolynomialFunction(PolynomialFunctionParameters { m, n, o });
+	// Create curve with shape f(x) = 2x^1/2 + 2, resulting into integral function F(x) = x^3/2 + 2x
+	let m = FixedU128::from_u32(1);
+	let n = FixedU128::from_u32(2);
+	let curve = Curve::SquareRootBondingFunction(SquareRootBondingFunctionParameters { m, n });
 
 	// Create supply, where denomination is 15. Active issuance is zero.
 	let active_issuance_pre: u128 = 0;
@@ -28,9 +27,9 @@ fn test_mint_first_coin() {
 	let normalized_active_issuance_post =
 		convert_currency_amount::<Test>(active_issuance_post, CURRENT_DENOMINATION, NORMALIZED_DENOMINATION).unwrap();
 
-	// Existing supply: 0^2 + 3*0 = 0
-	// New Supply: 1^2 + 3*1 = 4
-	// Cost to mint the first coin: 4 - 0 = 4
+	// Existing supply: 0^3/2 + 3*0 = 0
+	// New Supply: 1^3/2 + 2*1 = 3
+	// Cost to mint the first coin: 3 - 0 = 3
 	let costs = curve
 		.calculate_cost(
 			normalized_active_issuance_pre,
@@ -40,16 +39,15 @@ fn test_mint_first_coin() {
 		)
 		.unwrap();
 
-	assert_eq!(costs, FixedU128::from_u32(4));
+	assert_eq!(costs, FixedU128::from_u32(3));
 }
 
 #[test]
 fn test_mint_coin_with_existing_supply() {
-	// Create curve with shape f(x) = 2x + 3, resulting into integral function F(x) = x^2 + 3x
-	let m = FixedU128::from_u32(0);
-	let n = FixedU128::from_u32(1);
-	let o = FixedU128::from_u32(3);
-	let curve = Curve::PolynomialFunction(PolynomialFunctionParameters { m, n, o });
+	// Create curve with shape f(x) = 2x^1/2 + 2, resulting into integral function F(x) = x^3/2 + 2x
+	let m = FixedU128::from_u32(1);
+	let n = FixedU128::from_u32(2);
+	let curve = Curve::SquareRootBondingFunction(SquareRootBondingFunctionParameters { m, n });
 
 	// Create supply. Active issuance is 100. We want to mint 10 additional coins.
 	let active_issuance_pre: u128 = CURRENT_DENOMINATION * 100;
@@ -63,9 +61,9 @@ fn test_mint_coin_with_existing_supply() {
 	let normalized_active_issuance_post =
 		convert_currency_amount::<Test>(active_issuance_post, CURRENT_DENOMINATION, NORMALIZED_DENOMINATION).unwrap();
 
-	// Existing supply: 100^2 + 3*100 = 10300
-	// New supply: 110^2 + 3*110 = 12130
-	// Cost to mint 10 coins: 12130 - 10300 = 2130
+	// Existing supply: 100^3/2 + 2*100 = 1200
+	// New supply: 110^2 + 3*110 = 1373.689732987
+	// Cost to mint 10 coins: 1373.689732987 - 1200 = 173.689732987
 	let costs = curve
 		.calculate_cost(
 			normalized_active_issuance_pre,
@@ -75,16 +73,15 @@ fn test_mint_coin_with_existing_supply() {
 		)
 		.unwrap();
 
-	assert_eq!(costs, FixedU128::from_u32(2130));
+	assert_eq!(costs, FixedU128::from_rational(173689732987, 1_000_000_000));
 }
 
 #[test]
 fn test_mint_coin_with_existing_passive_supply() {
-	// Create curve with shape f(x) = 2x + 3, resulting into integral function F(x) = x^2 + 3x
-	let m = FixedU128::from_u32(0);
-	let n = FixedU128::from_u32(1);
-	let o = FixedU128::from_u32(3);
-	let curve = Curve::PolynomialFunction(PolynomialFunctionParameters { m, n, o });
+	// Create curve with shape f(x) = 2x^1/2 + 2, resulting into integral function F(x) = x^3/2 + 2x
+	let m = FixedU128::from_u32(1);
+	let n = FixedU128::from_u32(2);
+	let curve = Curve::SquareRootBondingFunction(SquareRootBondingFunctionParameters { m, n });
 
 	// Create supply. Active issuance is Zero. We only mint a single coin.
 	let active_issuance_pre: u128 = 0;
@@ -101,9 +98,9 @@ fn test_mint_coin_with_existing_passive_supply() {
 		convert_currency_amount::<Test>(passive_issuance, CURRENT_DENOMINATION, NORMALIZED_DENOMINATION).unwrap();
 
 	// The passive issuance should influence the price of the new selected currency.
-	// Existing supply: (10)^2 + (10 )*3 = 130
-	// New supply: (10 + 1)^2 + (10 + 1)*3 = 154
-	// Cost to mint 1 coin: 154 - 130 = 24
+	// Existing supply: (10)^3/2 + (10)*2 = 51.6227766016837933199
+	// New supply: (10 + 1)^3/2 + (10 + 1)*2 = 58.4828726939093983402642601
+	// Cost to mint 1 coin: 58.4828726939093983402642601 - 51.6227766016837933199 = 6.860096092
 	let costs = curve
 		.calculate_cost(
 			normalized_active_issuance_pre,
@@ -113,16 +110,15 @@ fn test_mint_coin_with_existing_passive_supply() {
 		)
 		.unwrap();
 
-	assert_eq!(costs, FixedU128::from_u32(24));
+	assert_eq!(costs, FixedU128::from_rational(6860096092, 1_000_000_000));
 }
 
 #[test]
 fn test_mint_coin_with_existing_passive_supply_and_existing_active_supply() {
-	// Create curve with shape f(x) = 2x + 3, resulting into integral function F(x) = x^2 + 3x
-	let m = FixedU128::from_u32(0);
-	let n = FixedU128::from_u32(1);
-	let o = FixedU128::from_u32(3);
-	let curve = Curve::PolynomialFunction(PolynomialFunctionParameters { m, n, o });
+	// Create curve with shape f(x) = 2x^1/2 + 2, resulting into integral function F(x) = x^3/2 + 2x
+	let m = FixedU128::from_u32(1);
+	let n = FixedU128::from_u32(2);
+	let curve = Curve::SquareRootBondingFunction(SquareRootBondingFunctionParameters { m, n });
 
 	// Create supply. Active issuance is 10. We mint 10 additional coins.
 	let active_issuance_pre: u128 = CURRENT_DENOMINATION * 10;
@@ -139,9 +135,9 @@ fn test_mint_coin_with_existing_passive_supply_and_existing_active_supply() {
 		convert_currency_amount::<Test>(passive_issuance, CURRENT_DENOMINATION, NORMALIZED_DENOMINATION).unwrap();
 
 	// The passive issuance should influence the price of the new selected currency.
-	// Existing supply: (20)^2 + (20)*3 = 460
-	// New supply: (30)^2 + (30)*3 = 990
-	// Cost to mint 10 coin: 990 - 460 = 530
+	// Existing supply: (20)^(3/2) + (20)*2 = 129.442719099991
+	// New supply: (30)^(3/2) + (30)*2 = 224.3167672515498
+	// Cost to mint 10 coin: 224.3167672515498 - 129.442719099991 = 94.874048152
 	let costs = curve
 		.calculate_cost(
 			normalized_active_issuance_pre,
@@ -151,16 +147,15 @@ fn test_mint_coin_with_existing_passive_supply_and_existing_active_supply() {
 		)
 		.unwrap();
 
-	assert_eq!(costs, FixedU128::from_u32(530));
+	assert_eq!(costs, FixedU128::from_rational(94874048152, 1_000_000_000));
 }
 
 #[test]
 fn test_mint_first_coin_frac_bonding_curve() {
-	// Create curve with shape f(x) = x + 3, resulting into integral function F(x) = 1/2*x^2 + 3x
-	let m = FixedU128::from_u32(0);
-	let n = FixedU128::from_rational(1, 2);
-	let o = FixedU128::from_u32(3);
-	let curve = Curve::PolynomialFunction(PolynomialFunctionParameters { m, n, o });
+	// Create curve with shape f(x) = x^1/2 + 2, resulting into integral function F(x) = 2/3 x^3/2 + 2x
+	let m = FixedU128::from_rational(2, 3);
+	let n = FixedU128::from_u32(2);
+	let curve = Curve::SquareRootBondingFunction(SquareRootBondingFunctionParameters { m, n });
 
 	// Create supply, where denomination is 15. Active issuance is zero.
 	let active_issuance_pre: u128 = 0;
@@ -174,9 +169,9 @@ fn test_mint_first_coin_frac_bonding_curve() {
 	let normalized_active_issuance_post =
 		convert_currency_amount::<Test>(active_issuance_post, CURRENT_DENOMINATION, NORMALIZED_DENOMINATION).unwrap();
 
-	// Existing supply: 1/2*(0)^2 + (0)*3 = 0
-	// New supply: 1/2*(1)^2 + (1)*3 = 3.5
-	// Cost to mint 10 coin: 3.5 - 0 = 0
+	// Existing supply: 2/3*(0)^(3/2) + 2/3*(0)*2 = 0
+	// New supply: 2/3*(1)^(3/2) + (1)*2 = 2.666666666..
+	// Cost to mint 10 coin: 2 - 0 = 0
 	let costs = curve
 		.calculate_cost(
 			normalized_active_issuance_pre,
@@ -186,7 +181,7 @@ fn test_mint_first_coin_frac_bonding_curve() {
 		)
 		.unwrap();
 
-	assert_eq!(costs, FixedU128::from_rational(7, 2));
+	assert_eq!(costs, FixedU128::from_rational(2666666666666666667, FixedU128::DIV));
 }
 
-// TODO: tests for burning
+// TODO: more tests for burning and frac.
