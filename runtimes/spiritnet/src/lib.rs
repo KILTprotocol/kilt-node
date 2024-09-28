e1da7f7cd7f073da78826e60b41cfbbe0d94fd62
Antonio Antonino <antonio@kilt.io>
2024-09-28 17:27:41 +0200

    chore: bump spiritnet runtime spec_version

--- runtimes/spiritnet/src/lib.rs
+++ runtimes/spiritnet/src/lib.rs
@@ -109,7 +109,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("kilt-spiritnet"),
 	impl_name: create_runtime_str!("kilt-spiritnet"),
 	authoring_version: 1,
-	spec_version: 11401,
+	spec_version: 11402,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 9,
