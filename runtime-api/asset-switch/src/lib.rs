78f348c7781e1187a32ee05cd43e63890e3060d4
Antonio Antonino <antonio@kilt.io>
2024-10-08 13:34:59 +0200

    Refactor XCM composition into its own function

--- runtime-api/asset-switch/src/lib.rs
+++ runtime-api/asset-switch/src/lib.rs
@@ -22,12 +22,16 @@ use parity_scale_codec::Codec;
 use sp_std::vec::Vec;
 
 sp_api::decl_runtime_apis! {
-	/// Runtime API to compute the pool account for a given switch pair ID and remote asset.
-	pub trait AssetSwitch<AssetId, AccountId, Error> where
+	/// Runtime API to compute the pool account for a given switch pair ID and remote asset, and to compute the XCM that would be sent to destination for a given switch operation.
+	pub trait AssetSwitch<AssetId, AccountId, Amount, Destination, Error, Xcm> where
 		AssetId: Codec,
 		AccountId: Codec,
+		Amount: Codec,
+		Destination: Codec,
 		Error: Codec,
+		Xcm: Codec,
 		{
 			fn pool_account_id(pair_id: Vec<u8>, asset_id: AssetId) -> Result<AccountId, Error>;
+			fn xcm_for_switch(pair_id: Vec<u8>, asset_id: AssetId, from: AccountId, to: Destination, amount: Amount) -> Result<Xcm, Error>;
 		}
 }
