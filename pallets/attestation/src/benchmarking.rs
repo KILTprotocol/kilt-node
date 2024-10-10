da7b68dcc7ca12fae79d14e4a8c558686a4bcb3e
Antonio Antonino <antonio@kilt.io>
2024-10-10 10:24:05 +0200

    fmt

--- pallets/attestation/src/benchmarking.rs
+++ pallets/attestation/src/benchmarking.rs
@@ -18,8 +18,7 @@
 
 use frame_benchmarking::{account, benchmarks};
 use frame_support::traits::{fungible::Mutate, Get};
-use frame_system::pallet_prelude::BlockNumberFor;
-use frame_system::RawOrigin;
+use frame_system::{pallet_prelude::BlockNumberFor, RawOrigin};
 use sp_runtime::traits::Hash;
 
 use ctype::CtypeEntryOf;
