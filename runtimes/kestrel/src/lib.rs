ecd212fac2a2bd56d8badb20a88cdc8a36a0fbbc
Antonio Antonino <antonio@kilt.io>
2024-10-10 10:53:48 +0200

    chore: bump versions

--- runtimes/kestrel/src/lib.rs
+++ runtimes/kestrel/src/lib.rs
@@ -120,7 +120,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("kilt-kestrel"),
 	impl_name: create_runtime_str!("kilt-kestrel"),
 	authoring_version: 4,
-	spec_version: 11500,
+	spec_version: 11405,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 6,
