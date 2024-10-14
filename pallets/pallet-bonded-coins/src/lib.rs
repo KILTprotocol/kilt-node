277b1f79254431f75578fc6e3ba77a40b056225d
Raphael <raphael@kilt.io>
2024-10-14 21:59:55 +0200

    feat: refund on teardown

--- pallets/pallet-bonded-coins/src/lib.rs
+++ pallets/pallet-bonded-coins/src/lib.rs
@@ -28,8 +28,9 @@ pub mod pallet {
 			fungibles::{
 				metadata::{Inspect as FungiblesInspect, Mutate as FungiblesMetadata},
 				Create as CreateFungibles, Destroy as DestroyFungibles, Inspect as InspectFungibles,
-				Mutate as MutateFungibles,
+				Mutate as MutateFungibles, Unbalanced,
 			},
+			tokens::{Fortitude, Precision, Preservation},
 			AccountTouch,
 		},
 		Parameter,
@@ -38,7 +39,7 @@ pub mod pallet {
 	use parity_scale_codec::FullCodec;
 	use scale_info::TypeInfo;
 	use sp_runtime::{
-		traits::{CheckedAdd, CheckedSub, One, Saturating, Zero},
+		traits::{Bounded, CheckedAdd, CheckedDiv, CheckedSub, One, SaturatedConversion, Saturating, Zero},
 		BoundedVec, FixedPointNumber,
 	};
 	use sp_std::default::Default;
@@ -147,13 +148,21 @@ pub mod pallet {
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
-		Todo,
+		/// A bonded token pool has been moved to destroying state. [pool_id]
+		DestructionStarted(T::PoolId),
+		/// A bonded token pool has been fully destroyed and all collateral and deposits have been refunded. [pool_id]
+		Destroyed(T::PoolId),
 	}
 
 	// Errors inform users that something went wrong.
 	#[pallet::error]
 	pub enum Error<T> {
-		Todo,
+		/// The pool id is not currently registered.
+		PoolUnknown,
+		/// The pool is already in the process of being destroyed.
+		Destroying,
+		/// The user is not privileged to perform the requested operation.
+		Unauthorized,
 	}
 
 	#[pallet::composite_enum]
@@ -199,32 +208,165 @@ pub mod pallet {
 			todo!()
 		}
 
-		// TODO: not sure if we really need that. Check that out with Raphael.
 		#[pallet::call_index(6)]
 		#[pallet::weight(Weight::from_parts(10_000, 0) + T::DbWeight::get().writes(1))]
-		pub fn start_destroy(_origin: OriginFor<T>) -> DispatchResult {
-			todo!()
+		pub fn start_destroy(origin: OriginFor<T>, pool_id: T::PoolId) -> DispatchResult {
+			let who = ensure_signed(origin)?;
+			let mut pool_details = Pools::<T>::get(pool_id.clone()).ok_or(Error::<T>::PoolUnknown)?;
+
+			ensure!(pool_details.is_manager(&who), Error::<T>::Unauthorized);
+
+			Self::do_start_destroy_pool(&mut pool_details)?;
+
+			Self::deposit_event(Event::DestructionStarted(pool_id));
+			Ok(())
 		}
 
 		#[pallet::call_index(7)]
 		#[pallet::weight(Weight::from_parts(10_000, 0) + T::DbWeight::get().writes(1))]
-		pub fn force_start_destroy(_origin: OriginFor<T>) -> DispatchResult {
-			todo!()
+		pub fn force_start_destroy(origin: OriginFor<T>, pool_id: T::PoolId) -> DispatchResult {
+			ensure_root(origin)?;
+
+			let mut pool_details = Pools::<T>::get(pool_id.clone()).ok_or(Error::<T>::PoolUnknown)?;
+
+			Self::do_start_destroy_pool(&mut pool_details)?;
+
+			Self::deposit_event(Event::DestructionStarted(pool_id));
+			Ok(())
 		}
 
-		// todo: check if we really need that tx.
 		#[pallet::call_index(8)]
 		#[pallet::weight(Weight::from_parts(10_000, 0) + T::DbWeight::get().writes(1))]
-		pub fn destroy_accounts(_origin: OriginFor<T>) -> DispatchResult {
-			todo!()
+		pub fn refund_accounts(origin: OriginFor<T>, pool_id: T::PoolId, max_accounts: u32) -> DispatchResult {
+			ensure_signed(origin)?;
+
+			let pool_details = Pools::<T>::get(pool_id.clone()).ok_or(Error::<T>::PoolUnknown)?;
+
+			ensure!(pool_details.state.is_destroying(), Error::<T>::Destroying); // TODO: incorrect error
+
+			let pool_account = pool_id.clone().into();
+
+			let total_collateral_issuance = Self::get_pool_collateral(&pool_account);
+
+			let total_issuances: Vec<FungiblesBalanceOf<T>> = if total_collateral_issuance.is_zero() {
+				// nothing to distribute
+				vec![Zero::zero(); pool_details.bonded_currencies.len()]
+			} else {
+				pool_details
+					.bonded_currencies
+					.iter()
+					.map(|id| T::Fungibles::total_issuance(id.clone()))
+					.collect()
+			};
+
+			let sum_of_issuances: FungiblesBalanceOf<T> = total_issuances
+				.iter()
+				.fold(Zero::zero(), |sum, x| sum.saturating_add(*x));
+			let collateral_per_token = total_collateral_issuance
+				.checked_div(&sum_of_issuances)
+				.unwrap_or(Zero::zero());
+
+			let mut remaining_max_accounts = max_accounts.clone();
+
+			for (idx, currency_id) in pool_details.bonded_currencies.clone().iter().enumerate() {
+				if !total_issuances[idx].is_zero() {
+					let refunded_accounts = Self::do_refund_accounts(
+						currency_id.clone(),
+						collateral_per_token,
+						remaining_max_accounts,
+						&pool_account,
+					)?;
+					if remaining_max_accounts > refunded_accounts {
+						remaining_max_accounts -= refunded_accounts;
+					} else {
+						// max_accounts reached; stop execution
+						return Ok(());
+					}
+				}
+				// issuance is zero or no more accounts; currency can be destroyed
+				T::Fungibles::start_destroy(currency_id.clone(), None)?; // TODO: can be called multiple times, but can we somehow avoid it?
+			}
+
+			Ok(())
 		}
 
 		#[pallet::call_index(9)]
 		#[pallet::weight(Weight::from_parts(10_000, 0) + T::DbWeight::get().writes(1))]
-		pub fn finish_destroy(_origin: OriginFor<T>) -> DispatchResult {
-			todo!()
+		pub fn finish_destroy(origin: OriginFor<T>, pool_id: T::PoolId) -> DispatchResult {
+			ensure_signed(origin)?;
+
+			let pool_details = Pools::<T>::get(pool_id.clone()).ok_or(Error::<T>::PoolUnknown)?;
+
+			ensure!(pool_details.state.is_destroying(), Error::<T>::Destroying);
+
+			for currency_id in pool_details.bonded_currencies {
+				if T::Fungibles::asset_exists(currency_id.clone()) {
+					T::Fungibles::finish_destroy(currency_id.clone())?;
+				}
+			}
+
+			let pool_account = pool_id.clone().into();
+
+			let total_collateral_issuance = Self::get_pool_collateral(&pool_account);
+
+			T::CollateralCurrency::transfer(
+				T::CollateralAssetId::get(),
+				&pool_account,
+				&pool_details.manager,
+				total_collateral_issuance,
+				Preservation::Expendable,
+			)?;
+
+			Pools::<T>::remove(&pool_id);
+
+			Self::deposit_event(Event::Destroyed(pool_id));
+
+			Ok(())
 		}
 	}
 
-	impl<T: Config> Pallet<T> {}
+	impl<T: Config> Pallet<T> {
+		fn do_start_destroy_pool(pool_details: &mut PoolDetailsOf<T>) -> DispatchResult {
+			ensure!(!pool_details.state.is_destroying(), Error::<T>::Destroying);
+
+			pool_details.state.destroy();
+
+			Ok(())
+		}
+
+		fn do_refund_accounts(
+			currency_id: FungiblesAssetIdOf<T>,
+			collateral_per_token: CollateralCurrencyBalanceOf<T>,
+			remaining_max_accounts: u32,
+			collateral_account: &AccountIdOf<T>,
+		) -> Result<u32, DispatchError> {
+			// TODO: how do we get the accounts?
+			let accounts = vec![];
+
+			for who in accounts.iter() {
+				let burnt = T::Fungibles::decrease_balance(
+					currency_id.clone(),
+					who,
+					Bounded::max_value(),
+					Precision::BestEffort,
+					Preservation::Expendable,
+					Fortitude::Force,
+				)?;
+
+				T::CollateralCurrency::transfer(
+					T::CollateralAssetId::get(),
+					collateral_account,
+					who,
+					burnt.saturating_mul(collateral_per_token.clone()),
+					Preservation::Expendable,
+				)?;
+			}
+
+			Ok(accounts.len().saturated_into())
+		}
+
+		fn get_pool_collateral(pool_account: &AccountIdOf<T>) -> CollateralCurrencyBalanceOf<T> {
+			T::CollateralCurrency::total_balance(T::CollateralAssetId::get(), pool_account)
+		}
+	}
 }
