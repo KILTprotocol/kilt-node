9dc4d0d92538bacebbdf25c1e4bd5892a31f7c1d
Adel Golghalyani <48685760+Ad96el@users.noreply.github.com>
2024-10-25 14:37:35 +0200

    Update pallets/pallet-bonded-coins/src/traits.rs
    Co-authored-by: Antonio <antonio@kilt.io>

--- pallets/pallet-bonded-coins/src/traits.rs
+++ pallets/pallet-bonded-coins/src/traits.rs
@@ -3,5 +3,5 @@ use frame_support::dispatch::DispatchResult;
 pub trait FreezeAccounts<AccountId, AssetId> {
 	fn freeze(caller: &AccountId, who: &AccountId, asset_id: &AssetId) -> DispatchResult;
 
-	fn thaw(caller: AccountId, who: AccountId, asset_id: AssetId) -> DispatchResult;
+	fn thaw(caller: &AccountId, who: &AccountId, asset_id: &AssetId) -> DispatchResult;
 }
