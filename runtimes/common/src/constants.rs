9e5e3f3064d9252c6314bb3fcf531e1f052501f5
Antonio Antonino <antonio@kilt.io>
2024-11-05 15:02:22 +0100

    Ignore clippy lint

--- runtimes/common/src/constants.rs
+++ runtimes/common/src/constants.rs
@@ -514,7 +514,9 @@ pub mod dot_names {
 	const MAX_NAME_LENGTH: u32 = 28;
 	const DOT_NAME_SUFFIX: &str = ".dot";
 
+	#[allow(clippy::as_conversions)]
 	pub const MIN_LENGTH: u32 = MIN_NAME_LENGTH + DOT_NAME_SUFFIX.len() as u32;
+	#[allow(clippy::as_conversions)]
 	pub const MAX_LENGTH: u32 = MAX_NAME_LENGTH + DOT_NAME_SUFFIX.len() as u32;
 
 	/// The size is checked in the runtime by a test.
