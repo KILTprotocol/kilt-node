cde18ad0b455ce0c8149e83728915a255066ea3c
Adel Golghalyani <ad96el@gmail.com>
2024-11-29 23:45:43 +0100

    fmt

--- pallets/pallet-bonded-coins/src/benchmarking.rs
+++ pallets/pallet-bonded-coins/src/benchmarking.rs
@@ -837,9 +837,9 @@ mod benchmarks {
 
 	#[benchmark]
 	fn force_start_refund(c: Linear<1, { T::MaxCurrencies::get() }>) {
-		let bonded_currencies = create_bonded_currencies_in_range::<T>(c, false);   
+		let bonded_currencies = create_bonded_currencies_in_range::<T>(c, false);
 		let target_asset_id = bonded_currencies[0].clone();
- 
+
 		let curve = get_lmsr_curve::<CurveParameterTypeOf<T>>();
 
 		let pool_id = create_pool::<T>(curve, bonded_currencies, None, None, None);
