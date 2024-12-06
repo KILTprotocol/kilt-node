77fa283a57a76a3ddc70373a2978d21267a989a8
Adel Golghalyani <ad96el@gmail.com>
2024-12-06 10:57:43 +0100

    remove duplicate types

--- runtimes/common/src/bonded_currencies/runtime_api.rs
+++ runtimes/common/src/bonded_currencies/runtime_api.rs
@@ -16,48 +16,8 @@
 
 // If you feel like getting in touch with us, you can do so at info@botlabs.org
 
-use pallet_bonded_coins::{curves::Curve, PoolDetails};
-use parity_scale_codec::{alloc::string::String, Decode, Encode};
+use parity_scale_codec::{Decode, Encode};
 use scale_info::TypeInfo;
-use sp_std::vec::Vec;
-use xcm::v4::AssetId as AssetIdLocation;
-
-use crate::{bonded_currencies::AssetId, AccountId, Balance};
-
-/// Enum to represent the operation of minting or burning tokens.
-#[derive(Decode, Encode, TypeInfo)]
-pub enum Operation {
-	Mint,
-	Burn,
-}
-
-/// Human readable curve type.
-pub type HumanReadableCurve = Curve<String>;
-
-/// Human readable bonded currencies
-pub type HumanReadableCurrencies = Vec<BondedCurrencyDetails>;
-
-/// Human readable pool details.
-pub type HumanReadablePoolDetails =
-	PoolDetails<AccountId, HumanReadableCurve, HumanReadableCurrencies, CollateralDetails, Balance>;
-
-/// Collateral currency details used for the runtime API.
-#[derive(Decode, Encode, TypeInfo)]
-pub struct CollateralDetails {
-	pub id: AssetIdLocation,
-	pub name: String,
-	pub symbol: String,
-	pub denomination: u8,
-}
-
-/// Bonded currency details used for the runtime API.
-#[derive(Decode, Encode, TypeInfo)]
-pub struct BondedCurrencyDetails {
-	pub id: AssetId,
-	pub name: String,
-	pub symbol: String,
-	pub supply: Balance,
-}
 
 #[derive(Debug, Encode, Decode, TypeInfo, PartialEq, Eq, PartialOrd, Ord)]
 pub enum Error {
