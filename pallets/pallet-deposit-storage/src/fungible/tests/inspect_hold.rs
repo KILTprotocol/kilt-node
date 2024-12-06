0c51fd20e2685c108201e8cbe9d31f5d016b5e17
Antonio Antonino <antonio@kilt.io>
2024-12-06 15:29:32 +0100

    Fix bug

--- pallets/pallet-deposit-storage/src/fungible/tests/inspect_hold.rs
+++ pallets/pallet-deposit-storage/src/fungible/tests/inspect_hold.rs
@@ -16,12 +16,15 @@
 
 // If you feel like getting in touch with us, you can do so at info@botlabs.org
 
-use frame_support::traits::fungible::{InspectHold, MutateHold};
+use frame_support::traits::{
+	fungible::{InspectHold, MutateHold},
+	tokens::Precision,
+};
 use sp_runtime::{traits::Zero, AccountId32};
 
 use crate::{
-	fungible::tests::mock::{DepositNamespace, ExtBuilder, TestRuntime, OTHER_ACCOUNT, OWNER},
-	Pallet, PalletDepositStorageReason,
+	fungible::tests::mock::{Balances, DepositNamespace, ExtBuilder, TestRuntime, OTHER_ACCOUNT, OWNER},
+	HoldReason, Pallet, PalletDepositStorageReason,
 };
 
 #[test]
@@ -56,5 +59,51 @@ fn balance_on_hold() {
 			let balance_on_hold_for_different_reason_different_user =
 				<Pallet<TestRuntime> as InspectHold<AccountId32>>::balance_on_hold(&other_reason, &OTHER_ACCOUNT);
 			assert!(balance_on_hold_for_different_reason_different_user.is_zero());
+		});
+}
+
+#[test]
+fn multiple_holds() {
+	ExtBuilder::default()
+		.with_balances(vec![(OWNER, 100_000)])
+		.build()
+		.execute_with(|| {
+			let reason = PalletDepositStorageReason {
+				namespace: DepositNamespace::ExampleNamespace,
+				key: [0].to_vec().try_into().unwrap(),
+			};
+			let other_reason = PalletDepositStorageReason {
+				namespace: DepositNamespace::ExampleNamespace,
+				key: [1].to_vec().try_into().unwrap(),
+			};
+
+			<Pallet<TestRuntime> as MutateHold<AccountId32>>::hold(&reason, &OWNER, 10)
+				.expect("Failed to hold amount for user.");
+			// The two different reasons should be stored under the same runtime reason in
+			// the underlying `Currency`.
+			assert_eq!(
+				<Balances as InspectHold<AccountId32>>::balance_on_hold(&HoldReason::Deposit.into(), &OWNER),
+				10
+			);
+
+			<Pallet<TestRuntime> as MutateHold<AccountId32>>::hold(&other_reason, &OWNER, 15)
+				.expect("Failed to hold amount for user.");
+			assert_eq!(
+				<Balances as InspectHold<AccountId32>>::balance_on_hold(&HoldReason::Deposit.into(), &OWNER),
+				25
+			);
+
+			<Pallet<TestRuntime> as MutateHold<AccountId32>>::release(&other_reason, &OWNER, 15, Precision::Exact)
+				.expect("Failed to release amount for user.");
+			assert_eq!(
+				<Balances as InspectHold<AccountId32>>::balance_on_hold(&HoldReason::Deposit.into(), &OWNER),
+				10
+			);
+
+			<Pallet<TestRuntime> as MutateHold<AccountId32>>::release(&reason, &OWNER, 10, Precision::Exact)
+				.expect("Failed to release amount for user.");
+			assert!(
+				<Balances as InspectHold<AccountId32>>::balance_on_hold(&HoldReason::Deposit.into(), &OWNER).is_zero()
+			);
 		});
 }
