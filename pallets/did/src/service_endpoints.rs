2e6aa807ef86ea4b7807287362c9d860560e19dc
Adel Golghalyani <48685760+Ad96el@users.noreply.github.com>
2025-02-06 16:43:31 +0100

    chore: licence (#860)
    KILT Foundation

--- pallets/did/src/service_endpoints.rs
+++ pallets/did/src/service_endpoints.rs
@@ -1,5 +1,5 @@
-// KILT Blockchain – https://botlabs.org
-// Copyright (C) 2019-2024 BOTLabs GmbH
+// KILT Blockchain – <https://kilt.io>
+// Copyright (C) 2025, KILT Foundation
 
 // The KILT Blockchain is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-// If you feel like getting in touch with us, you can do so at info@botlabs.org
+// If you feel like getting in touch with us, you can do so at <hello@kilt.org>
 
 use crate::{errors, Config};
 use frame_support::{ensure, traits::Get, BoundedVec};
