5377e4f4da6347cc8db3ea3cee73b12ea8009bd8
Adel Golghalyani <ad96el@gmail.com>
2025-02-11 14:12:39 +0100

    remove comments

--- runtimes/spiritnet/src/lib.rs
+++ runtimes/spiritnet/src/lib.rs
@@ -160,8 +160,6 @@ construct_runtime! {
 		Migration: pallet_migration = 70,
 		DipProvider: pallet_dip_provider = 71,
 		DepositStorage: pallet_deposit_storage = 72,
-		// DELETED: DotNames: pallet_web3_names::<Instance2> = 73,
-		// DELETED: UniqueLinking: pallet_did_lookup::<Instance2> = 74,
 
 		// Parachains pallets. Start indices at 80 to leave room.
 
