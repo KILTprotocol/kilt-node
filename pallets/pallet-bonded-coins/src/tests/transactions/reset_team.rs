cc4475c68a6af9118e5b07cc5dd3d08cadb87834
Raphael Flechtner <39338561+rflechtner@users.noreply.github.com>
2025-03-06 16:17:57 +0100

    refactor: batch updating of asset team (#869)
    fixes audit issue 6

--- pallets/pallet-bonded-coins/src/tests/transactions/reset_team.rs
+++ pallets/pallet-bonded-coins/src/tests/transactions/reset_team.rs
@@ -51,13 +51,57 @@ fn resets_team() {
 					admin: ACCOUNT_00,
 					freezer: ACCOUNT_01,
 				},
-				0
+				1
 			));
 
 			assert_eq!(Assets::admin(DEFAULT_BONDED_CURRENCY_ID), Some(ACCOUNT_00));
 			assert_eq!(Assets::freezer(DEFAULT_BONDED_CURRENCY_ID), Some(ACCOUNT_01));
 			assert_eq!(Assets::owner(DEFAULT_BONDED_CURRENCY_ID), Some(pool_id.clone()));
 			assert_eq!(Assets::issuer(DEFAULT_BONDED_CURRENCY_ID), Some(pool_id));
+		})
+}
+
+#[test]
+fn resets_team_for_all() {
+	let currencies = vec![DEFAULT_BONDED_CURRENCY_ID, DEFAULT_BONDED_CURRENCY_ID + 1];
+
+	let pool_details = generate_pool_details(
+		currencies.clone(),
+		get_linear_bonding_curve(),
+		false,
+		Some(PoolStatus::Active),
+		Some(ACCOUNT_00),
+		None,
+		None,
+		None,
+	);
+	let pool_id: AccountIdOf<Test> = calculate_pool_id(&currencies);
+
+	ExtBuilder::default()
+		.with_pools(vec![(pool_id.clone(), pool_details)])
+		.with_collaterals(vec![DEFAULT_COLLATERAL_CURRENCY_ID])
+		.build_and_execute_with_sanity_tests(|| {
+			let manager_origin = RawOrigin::Signed(ACCOUNT_00).into();
+
+			assert_ok!(BondingPallet::reset_team(
+				manager_origin,
+				pool_id.clone(),
+				PoolManagingTeam {
+					admin: ACCOUNT_00,
+					freezer: ACCOUNT_01,
+				},
+				2
+			));
+
+			assert_eq!(Assets::admin(currencies[0]), Some(ACCOUNT_00));
+			assert_eq!(Assets::freezer(currencies[0]), Some(ACCOUNT_01));
+			assert_eq!(Assets::owner(currencies[0]), Some(pool_id.clone()));
+			assert_eq!(Assets::issuer(currencies[0]), Some(pool_id.clone()));
+
+			assert_eq!(Assets::admin(currencies[1]), Some(ACCOUNT_00));
+			assert_eq!(Assets::freezer(currencies[1]), Some(ACCOUNT_01));
+			assert_eq!(Assets::owner(currencies[1]), Some(pool_id.clone()));
+			assert_eq!(Assets::issuer(currencies[1]), Some(pool_id));
 		})
 }
 
@@ -89,7 +133,7 @@ fn does_not_change_team_when_not_live() {
 						admin: ACCOUNT_00,
 						freezer: ACCOUNT_00,
 					},
-					0
+					1
 				),
 				BondingPalletErrors::<Test>::PoolNotLive
 			);
@@ -130,7 +174,7 @@ fn only_manager_can_change_team() {
 						admin: ACCOUNT_00,
 						freezer: ACCOUNT_00,
 					},
-					0
+					1
 				),
 				BondingPalletErrors::<Test>::NoPermission
 			);
@@ -143,7 +187,7 @@ fn only_manager_can_change_team() {
 						admin: ACCOUNT_00,
 						freezer: ACCOUNT_00,
 					},
-					0
+					1
 				),
 				BondingPalletErrors::<Test>::NoPermission
 			);
@@ -153,7 +197,7 @@ fn only_manager_can_change_team() {
 }
 
 #[test]
-fn handles_currency_idx_out_of_bounds() {
+fn handles_currency_number_incorrect() {
 	let pool_details = generate_pool_details(
 		vec![DEFAULT_BONDED_CURRENCY_ID],
 		get_linear_bonding_curve(),
@@ -180,9 +224,9 @@ fn handles_currency_idx_out_of_bounds() {
 						admin: ACCOUNT_00,
 						freezer: ACCOUNT_00,
 					},
-					2
+					0
 				),
-				BondingPalletErrors::<Test>::IndexOutOfBounds
+				BondingPalletErrors::<Test>::CurrencyCount
 			);
 		})
 }
