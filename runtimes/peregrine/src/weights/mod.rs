8adb7fd3ec54ca22b968613ea6b2ba20968a5145
Adel Golghalyani <ad96el@gmail.com>
2025-04-24 10:43:39 +0200

    remove weights inflation

--- runtimes/peregrine/src/weights/mod.rs
+++ runtimes/peregrine/src/weights/mod.rs
@@ -34,7 +34,6 @@ pub mod pallet_deposit_storage;
 pub mod pallet_did_lookup;
 pub mod pallet_dip_provider;
 pub mod pallet_indices;
-pub mod pallet_inflation;
 pub mod pallet_membership;
 pub mod pallet_message_queue;
 pub mod pallet_migration;
