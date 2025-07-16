2914cfd7c2e30893a37d620fdafcd5598422449a
Raphael <raphael@kilt.io>
2025-07-16 13:42:35 +0200

    chore: bump spec versions

--- runtimes/kestrel/src/lib.rs
+++ runtimes/kestrel/src/lib.rs
@@ -124,7 +124,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("kilt-kestrel"),
 	impl_name: create_runtime_str!("kilt-kestrel"),
 	authoring_version: 4,
-	spec_version: 11600,
+	spec_version: 11603,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 6,
